fd39547ac86420d0ea571c59a6166af1e9a64ffe
dominickasanga <dominickasanga@gmail.com>
2023-11-27 14:57:46 +0200

    modified:   package.json modified:   src/components/ConsultationPlan/Complications.vue

--- src/components/ConsultationPlan/Complications.vue
+++ src/components/ConsultationPlan/Complications.vue
@@ -1,15 +1,58 @@
 <template>
     <ion-list>
         <ion-item>
-            <ion-toggle>Visual acuity test</ion-toggle>
+            <ion-toggle :checked="showVisualAcuityTest" @ionChange="toggleShowVisualAcuityTest">Visual acuity test</ion-toggle>
         </ion-item>
+            <div class="sub_item_body" v-if="showVisualAcuityTest">
+                <ion-row>
+                    <ion-col>
+                        <div style="margin: 1%;">
+                            <ion-label style="margin-left: 3%;">Left Eye</ion-label>
+                            <ion-list>
+                                <ion-item class="item_eye_">
+                                    <ion-select 
+                                        label="---"
+                                        aria-label=""
+                                        placeholder="select complaints"
+                                        :multiple="true"
+                                        @ionChange="leftEyeComplaintSelect($event)"
+                                        @ionCancel="leftEyeHandleCancel()"
+                                        @ionDismiss="leftEyeHandleDismiss()"
+                                        fill="outline"
+                                        class="_item_eye"
+                                        
+                                    >
+                                        <ion-select-option v-for="item in complaintsList" :value="item">{{ item }}</ion-select-option>
+                                    </ion-select>
+                                </ion-item>
+                            </ion-list>
+                        </div>
+                    </ion-col>
+                    <ion-col>
+                        <div style="margin: 1%;">
+                            <ion-label style="margin-left: 3%;">Right Eye</ion-label>
+                            <ion-list>
+                                <ion-item class="item_eye_">
+                                    <ion-select fill="outline" class="_item_eye" label="---" aria-label="Fruit" placeholder="select complaints" :multiple="true">
+                                        <div v-for="(item, index) in complaintsList" :key="index">
+                                            <ion-select-option :value="item">{{ item }}</ion-select-option>
+                                        </div>
+                                    </ion-select>
+                                </ion-item>
+                            </ion-list>
+                        </div>
+                    </ion-col>
+                </ion-row>
+            </div>
+
         <ion-item>
             <ion-toggle>Retinopathy screening</ion-toggle>            
         </ion-item>
+
         <ion-item>
             <ion-toggle :checked="footChecked" @ionChange="footScreening">Foot screening</ion-toggle>
         </ion-item>
-        <div class="foot_body" v-if="footChecked">
+        <div class="sub_item_body" v-if="footChecked">
             <ion-row class="foot_title">
                 <ion-col class="first_col">Physical Exam</ion-col>
                 <ion-col>Left foot</ion-col>
@@ -173,7 +216,9 @@
             IonToolbar, 
             IonMenu,
             menuController,
-            IonToggle 
+            IonToggle,
+            IonSelectOption,
+            IonSelect,
         } from '@ionic/vue';
     import { defineComponent } from 'vue';
     import { checkmark,pulseOutline } from 'ionicons/icons';
@@ -190,12 +235,27 @@
         IonMenu,
         IonTitle,
         IonToolbar,
-        IonToggle
+        IonToggle,
+        IonSelect,
+        IonSelectOption,
         },
         data() {
     return {
         iconsContent: icons,
-        footChecked : false
+        footChecked : false,
+        showVisualAcuityTest: false,
+        complaintsList: [
+            "Redness",
+            "Itching",
+            "Burning",
+            "Blurry Vision",
+            "Dryness",
+            "Tearing",
+            "Sensitivity to Light",
+            "Swelling",
+            "Foreign Body Sensation",
+            "Pain"
+            ],
     };
   },
     setup() {
@@ -208,8 +268,19 @@
         },
         footScreening(){
             this.footChecked = !this.footChecked
+        },
+        toggleShowVisualAcuityTest() {
+            this.showVisualAcuityTest = !this.showVisualAcuityTest
+        },
+        leftEyeComplaintSelect(ev: any) {
+            console.log('ionChange fired with value: ' + ev.detail.value);
+        },
+        leftEyeHandleCancel() {
+
+        },
+        leftEyeHandleDismiss() {
+
         }
-        
     }
     });
 </script>
@@ -253,7 +324,7 @@ text-decoration: none;
 .first_col{
     text-align: left;
 }
-.foot_body{
+.sub_item_body{
     margin-left: 45px;
 }
 .foot_input{
@@ -262,8 +333,22 @@ text-decoration: none;
     text-align: left;
 
 }
+.item-content {
+  background-color:#ffffff;
+}
 .input_item {
   border: 1px solid transparent;
 }
+ion-select._item_eye {
+    --background: #fff;
+    
+}
+ion-item.item_eye_ {
+    --inner-border-width:0;
+    --background-hover: none;
+}
+/* ion-toggle {
+    --track-background-checked: #006401
+} */
 </style>
   
