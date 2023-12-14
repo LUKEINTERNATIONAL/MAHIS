181434afcdae176739576e4bb54d829e4962b050
dominickasanga <dominickasanga@gmail.com>
2023-12-14 20:23:34 +0200

    new file:   src/stores/TreatmentPlanStore.ts

---
+++ src/stores/TreatmentPlanStore.ts
@@ -0,0 +1,15 @@
+import { defineStore } from 'pinia'
+
+export const useDiagnosisStore = defineStore('TreatmentPlanStore', {
+    state: () => ({
+        diagnosis: [] as any,
+        
+    }),
+    actions:{
+        setDiagnosis(data: any){
+            this.diagnosis = data
+        }
+    },
+    persist:true,
+
+})
