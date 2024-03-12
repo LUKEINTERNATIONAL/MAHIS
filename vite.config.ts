a6307e135be29ca2887506e413dad10d42356ffb
petroskayange <kayangepetros@gmail.com>
2024-03-12 10:35:21 +0200

    changing configurations

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,25 @@
-import legacy from '@vitejs/plugin-legacy'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
-import { defineConfig } from 'vite'
-import { visualizer } from 'rollup-plugin-visualizer';
+import legacy from "@vitejs/plugin-legacy";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import { defineConfig } from "vite";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    legacy()
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+    plugins: [vue(), legacy()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom'
-  },
-  build: {
-    rollupOptions: {
-      plugins: [
-        visualizer(),
-      ],
+    test: {
+        globals: true,
+        environment: "jsdom",
     },
-  },
-  base: ''
-})
+    build: {
+        rollupOptions: {
+            plugins: [visualizer()],
+        },
+    },
+    base: "",
+});
