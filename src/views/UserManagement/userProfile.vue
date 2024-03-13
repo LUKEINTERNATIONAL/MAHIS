85fcf696171c7431785fc1768a3ea66a1187d5ab
dominickasanga <dominickasanga@gmail.com>
2024-03-13 12:04:11 +0200

    modified:   src/components/Toolbar.vue modified:   src/router/index.ts new file:   src/views/UserManagement/userProfile.vue

---
+++ src/views/UserManagement/userProfile.vue
@@ -0,0 +1,77 @@
+<template>
+    <ion-page>
+      <Toolbar />
+      <ion-content :fullscreen="true">
+        <div id="container">
+          
+       
+        
+            
+        </div>  
+      </ion-content>
+    </ion-page>
+  </template>
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+    watch: {},
+    name: "xxxComponent",
+});
+</script>
+<script setup lang="ts">
+import {
+    IonContent,
+    IonHeader,
+    IonCol,
+    IonItem,
+    IonList,
+    IonButton,
+    IonMenu,
+    IonTitle,
+    IonToolbar,
+    IonInput,
+    IonDatetime,
+    IonLabel,
+    IonTextarea,
+    IonAccordion,
+    IonAccordionGroup,
+    AccordionGroupCustomEvent,
+} from "@ionic/vue";
+import Toolbar from '@/components/Toolbar.vue'
+import ToolbarSearch from '@/components/ToolbarSearch.vue'
+</script>
+
+<style scoped>
+#container {
+  text-align: center;
+  
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+#container strong {
+  font-size: 20px;
+  line-height: 26px;
+}
+
+#container p {
+  font-size: 16px;
+  line-height: 22px;
+  
+  color: #8c8c8c;
+  
+  margin: 0;
+}
+
+#container a {
+  text-decoration: none;
+}
+.centered-content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
