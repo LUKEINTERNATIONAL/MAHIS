e854250416f8796cc06d442dc7f62fc25eaae66b
petroskayange <kayangepetros@gmail.com>
2024-03-26 12:22:12 +0200

    adding diagnosis

---
+++ src/apps/OPD/stores/DiagnosisStore.ts
@@ -0,0 +1,65 @@
+import { defineStore } from "pinia";
+import { icons } from "@/utils/svg";
+
+export const useOPDDiagnosisStore = defineStore("OPDDiagnosisStore", {
+    state: () => ({
+        OPDdiagnosis: [
+            {
+                selectedData: [],
+                isFinishBtn: false,
+                data: {
+                    rowData: [
+                        {
+                            colData: [
+                                {
+                                    inputHeader: "Primary Diagnosis",
+                                    icon: icons.search,
+                                    popOver: true,
+                                    value: "",
+                                    name: "diagnosis",
+                                    eventType: "input",
+                                    required: true,
+                                    alertsError: false,
+                                    alertsErrorMassage: "",
+                                    popOverData: {
+                                        filterData: false,
+                                        data: [],
+                                    },
+                                    id: "",
+                                    idName: "district_id",
+                                },
+                                {
+                                    inputHeader: "Differential Diagnosis",
+                                    icon: icons.search,
+                                    popOver: true,
+                                    value: "",
+                                    name: "diagnosis",
+                                    eventType: "input",
+                                    required: true,
+                                    alertsError: false,
+                                    alertsErrorMassage: "",
+                                    id: "",
+                                    idName: "district_id",
+                                },
+                            ],
+                            btns: [
+                                {
+                                    name: "Save",
+                                    fill: "clear",
+                                    btn_col_size: 3,
+                                    icon: icons.plus,
+                                },
+                            ],
+                        },
+                    ],
+                },
+            },
+        ] as any,
+    }),
+    actions: {
+        setOPDDiagnosis(data: any) {
+            this.OPDdiagnosis = data;
+        },
+    },
+    // persist: true,
+});
