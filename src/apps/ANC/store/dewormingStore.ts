c81661480b8baebf04744fb3796a77d7b83b0bed
Crispas Makanani <crispasmakanani@gmail.com>
2024-04-10 20:22:04 +0200

    concepts treatement

--- src/apps/ANC/store/dewormingStore.ts
+++ src/apps/ANC/store/dewormingStore.ts
@@ -2,227 +2,230 @@ import { defineStore } from "pinia";
 import { icons } from "@/utils/svg";
 
 export const useDewormingStore = defineStore("dewormingStore", {
-    state: () => ({
-        treatment: [
+  state: () => ({
+    treatment: [
+      {
+        selectdData: [],
+        isFinishBtn: false,
+        classDash: "dashed_bottom_border _padding",
+        radioBtnContent: {
+          header: {
+            title: "Was preventative antihelminthic treatment provided?",
+            selectedValue: "",
+            name: "antihelminthic treatment",
+          },
+          data: [
             {
-                selectdData: [],
-                isFinishBtn: false,
-                classDash: "dashed_bottom_border _padding",
-                radioBtnContent: {
-                    header: {
-                        title: "Was preventative antihelminthic treatment provided?",
-                        selectedValue: "",
-                        name: "preventive",
-                    },
-                    data: [
-                        {
-                            name: "Yes",
-                            value: "yes",
-                            labelPlacement: "start",
-                            colSize: "7",
-                            justify: "space-between",
-                        },
-                        {
-                            name: "No",
-                            value: "no",
-                            labelPlacement: "start",
-                            colSize: "7",
-                            justify: "space-between",
-                        },
-                    ],
-                },
-            },
-            // ],
-
-            // treatmentType:[
-            {
-                selectdData: [],
-                isFinishBtn: false,
-                radioBtnContent: {
-                    header: {
-                        title: "Preventative antihelminthic treatment provided",
-                        selectedValue: "",
-                        name: "treatInfo",
-                        displayNone: true,
-                    },
-                    data: [
-                        {
-                            name: "Single-dose albendazole 400mg",
-                            value: "single",
-                            labelPlacement: "start",
-                            colSize: "7",
-                            justify: "space-between",
-                        },
-                        {
-                            name: "Single-dose albendazole 500mg",
-                            value: "n",
-                            labelPlacement: "start",
-                            colSize: "7",
-                            justify: "space-between",
-                        },
-                    ],
-                },
-            },
-            // ],
-
-            // treatmentReason:[
-            {
-                selectdData: [],
-                isFinishBtn: false,
-                classDash: "",
-                radioBtnContent: {
-                    header: {
-                        title: "Reason no preventative treatment provided",
-                        selectedValue: "",
-                        name: "reason",
-                        displayNone: true,
-                    },
-                    data: [
-                        {
-                            name: "Client was referred",
-                            value: "client_referred",
-                            labelPlacement: "start",
-                            colSize: "7",
-                            justify: "space-between",
-                        },
-                        {
-                            name: "Other",
-                            value: "other",
-                            labelPlacement: "start",
-                            colSize: "7",
-                            justify: "space-between",
-                        },
-                    ],
-                },
+              name: "Yes",
+              value: "yes",
+              labelPlacement: "start",
+              colSize: "7",
+              justify: "space-between",
             },
             {
-                isFinishBtn: false,
-                sectionHeader: "",
-
-                data: {
-                    rowData: [
-                        {
-                            colData: [
-                                {
-                                    displayNone: true,
-                                    inputHeader: "specify",
-                                    value: "",
-                                    name: "Specify",
-                                    icon: icons.editPen,
-                                    required: true,
-                                    eventType: "input",
-                                    inputWidth: "85%",
-                                    alertsError: false,
-                                    alertsErrorMassage: "",
-                                },
-                            ],
-                        },
-                    ],
-                },
+              name: "No",
+              value: "no",
+              labelPlacement: "start",
+              colSize: "7",
+              justify: "space-between",
             },
-        ],
-
-        malaria: [
-            {
-                selectdData: [],
-                isFinishBtn: false,
-                classDash: "dashed_bottom_border _padding",
-                radioBtnContent: {
-                    header: {
-                        title: "Was preventative antihelminthic treatment provided?",
-                        selectedValue: "",
-                    },
-                    data: [
-                        {
-                            name: "Yes",
-                            value: "y",
-                            labelPlacement: "start",
-                            colSize: "7",
-                            justify: "space-between",
-                        },
-                        {
-                            name: "No",
-                            value: "n",
-                            labelPlacement: "start",
-                            colSize: "7",
-                            justify: "space-between",
-                        },
-                    ],
-                },
-            },
-        ],
-
-        malariaReason: [
-            {
-                selectdData: [],
-                isFinishBtn: false,
-                classDash: "",
-                radioBtnContent: {
-                    header: {
-                        title: "Reason no counselling on malaria prevention was not conducted",
-                        selectedValue: "",
-                    },
-                    data: [
-                        {
-                            name: "Client was referred",
-                            value: "client_referred",
-                            labelPlacement: "start",
-                            colSize: "7",
-                            justify: "space-between",
-                        },
-                        {
-                            name: "Other",
-                            value: "other",
-                            labelPlacement: "start",
-                            colSize: "7",
-                            justify: "space-between",
-                        },
-                    ],
-                },
-            },
-            {
-                isFinishBtn: false,
-                sectionHeader: "",
-                classDash: "dashed_bottom_border _padding",
-
-                data: {
-                    rowData: [
-                        {
-                            colData: [
-                                {
-                                    inputHeader: "Other (Specify)",
-                                    value: "",
-                                    name: "other",
-                                    icon: icons.editPen,
-                                    required: true,
-                                    eventType: "input",
-                                    inputWidth: "85%",
-                                    alertsError: false,
-                                    alertsErrorMassage: "",
-                                },
-                            ],
-                        },
-                    ],
-                },
-            },
-        ],
-    }),
-    actions: {
-        setTreatment(data: any) {
-            this.treatment = data;
+          ],
         },
-        setTreatmentType(data: any) {
-            // this.treatmentType = data
+      },
+      // ],
+
+      // treatmentType:[
+      {
+        selectdData: [],
+        isFinishBtn: false,
+        radioBtnContent: {
+          header: {
+            title: "Preventative antihelminthic treatment provided",
+            selectedValue: "",
+            name: "antihelminthic treatment provided",
+            displayNone: true,
+          },
+          data: [
+            {
+              name: "Single-dose albendazole 400mg",
+              value: "single",
+              labelPlacement: "start",
+              colSize: "7",
+              justify: "space-between",
+            },
+            {
+              name: "Single-dose albendazole 500mg",
+              value: "n",
+              labelPlacement: "start",
+              colSize: "7",
+              justify: "space-between",
+            },
+          ],
         },
-        setTreatmentReason(data: any) {
-            // this.treatmentReason = data
+      },
+      // ],
+
+      // treatmentReason:[
+      {
+        selectdData: [],
+        isFinishBtn: false,
+        classDash: "",
+        radioBtnContent: {
+          header: {
+            title: "Reason no preventative treatment provided",
+            selectedValue: "",
+            name: "no preventative treatment provided",
+            displayNone: true,
+          },
+          data: [
+            {
+              name: "Client was referred",
+              value: "client_referred",
+              labelPlacement: "start",
+              colSize: "7",
+              justify: "space-between",
+            },
+            {
+              name: "Other",
+              value: "other",
+              labelPlacement: "start",
+              colSize: "7",
+              justify: "space-between",
+            },
+          ],
         },
-        setMalaria(data: any) {
-            this.malaria = data;
+      },
+      {
+        isFinishBtn: false,
+        sectionHeader: "",
+
+        data: {
+          rowData: [
+            {
+              colData: [
+                {
+                  displayNone: true,
+                  inputHeader: "specify",
+                  value: "",
+                  name: "Specify",
+                  icon: icons.editPen,
+                  required: true,
+                  eventType: "input",
+                  inputWidth: "85%",
+                  alertsError: false,
+                  alertsErrorMassage: "",
+                },
+              ],
+            },
+          ],
         },
-        setMalariaReason(data: any) {
-            this.malariaReason = data;
+      },
+    ],
+
+    malaria: [
+      {
+        selectdData: [],
+        isFinishBtn: false,
+        classDash: "dashed_bottom_border _padding",
+        radioBtnContent: {
+          header: {
+            title: "Was preventative antihelminthic treatment provided?",
+            selectedValue: "",
+            name: " preventative antihelminthic treatment",
+          },
+          data: [
+            {
+              name: "Yes",
+              value: "y",
+              labelPlacement: "start",
+              colSize: "7",
+              justify: "space-between",
+            },
+            {
+              name: "No",
+              value: "n",
+              labelPlacement: "start",
+              colSize: "7",
+              justify: "space-between",
+            },
+          ],
         },
+      },
+    ],
+
+    malariaReason: [
+      {
+        selectdData: [],
+        isFinishBtn: false,
+        classDash: "",
+        radioBtnContent: {
+          header: {
+            title:
+              "Reason no counselling on malaria prevention was not conducted",
+            selectedValue: "",
+            name: "counselling on malaria prevention",
+          },
+          data: [
+            {
+              name: "Client was referred",
+              value: "client_referred",
+              labelPlacement: "start",
+              colSize: "7",
+              justify: "space-between",
+            },
+            {
+              name: "Other",
+              value: "other",
+              labelPlacement: "start",
+              colSize: "7",
+              justify: "space-between",
+            },
+          ],
+        },
+      },
+      {
+        isFinishBtn: false,
+        sectionHeader: "",
+        classDash: "dashed_bottom_border _padding",
+
+        data: {
+          rowData: [
+            {
+              colData: [
+                {
+                  inputHeader: "Other (Specify)",
+                  value: "",
+                  name: "other",
+                  icon: icons.editPen,
+                  required: true,
+                  eventType: "input",
+                  inputWidth: "85%",
+                  alertsError: false,
+                  alertsErrorMassage: "",
+                },
+              ],
+            },
+          ],
+        },
+      },
+    ],
+  }),
+  actions: {
+    setTreatment(data: any) {
+      this.treatment = data;
     },
-    persist: true,
+    setTreatmentType(data: any) {
+      // this.treatmentType = data
+    },
+    setTreatmentReason(data: any) {
+      // this.treatmentReason = data
+    },
+    setMalaria(data: any) {
+      this.malaria = data;
+    },
+    setMalariaReason(data: any) {
+      this.malariaReason = data;
+    },
+  },
+  persist: true,
 });
