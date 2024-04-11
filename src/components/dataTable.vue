554b0b2f983e41f1a85696e4d11848632a179720
dominickasanga <dominickasanga@gmail.com>
2024-04-11 20:21:31 +0200

    modified:   src/components/dataTable.vue

--- src/components/dataTable.vue
+++ src/components/dataTable.vue
@@ -1,5 +1,5 @@
 <template>
-        <ion-card>
+        <ion-card style="margin-top: 0px;">
           <ion-row>
 
             <ion-col>
@@ -30,6 +30,7 @@
           </ion-row>
 
             <EasyDataTable
+                table-class-name="customize-table"
                 :headers="headers"
                 :items="items_local"
                 :rows-per-page="10"
@@ -52,19 +53,23 @@
 
                 <ion-row class="spc_btwn" v-if="dynamic_button_properties[0].showAddItemButton">
                   <dynamic-button
+                  class="btn-cls"
                     v-if="dynamic_button_properties[0].addItemButton"
                         :name="dynamic_button_properties[0].name"
                         :fill="dynamic_button_properties[0].btnFill"
                         :icon="returnUpBackOutline"
                         @clicked:btn="prevPage"
+                        :color="''"
                   />
 
                   <dynamic-button
+                  class="btn-cls"
                     v-if="dynamic_button_properties[1].addItemButton"
                         :name="dynamic_button_properties[1].name"
                         :fill="dynamic_button_properties[1].btnFill"
                         :icon="returnUpForwardOutline"
                         @clicked:btn="nextPage"
+                        :color="''"
                   />
                 </ion-row>
               </template>
@@ -229,3 +234,54 @@ const showRow = (item: ClickRowArgument) => {
   clickRow(item)
 }
 </script>
+<style scoped>
+  .customize-table {
+  /* --easy-table-border: 1px solid #445269; */
+  --easy-table-row-border: 1px solid #445269;
+
+  --easy-table-header-font-size: 14px;
+  --easy-table-header-height: 50px;
+  --easy-table-header-font-color: #c1cad4;
+  --easy-table-header-background-color: #006401;
+
+  --easy-table-header-item-padding: 10px 15px;
+
+  --easy-table-body-even-row-font-color: #fff;
+  --easy-table-body-even-row-background-color: #4c5d7a;
+
+  --easy-table-body-row-font-color: #c0c7d2;
+  /* --easy-table-body-row-background-color: #2d3a4f; */
+  --easy-table-body-row-height: 50px;
+  --easy-table-body-row-font-size: 17px;
+
+  /* --easy-table-body-row-hover-font-color: #2d3a4f; */
+  --easy-table-body-row-hover-background-color: #eee;
+
+  --easy-table-body-item-padding: 10px 15px;
+
+  --easy-table-footer-background-color: #1e8635;
+  --easy-table-footer-font-color: #c0c7d2;
+  --easy-table-footer-font-size: 18px;
+  --easy-table-footer-padding: 0px 10px;
+  --easy-table-footer-height: 50px;
+
+  --easy-table-rows-per-page-selector-width: 70px;
+  --easy-table-rows-per-page-selector-option-padding: 10px;
+  --easy-table-rows-per-page-selector-z-index: 1;
+
+
+  --easy-table-scrollbar-track-color: #2d3a4f;
+  --easy-table-scrollbar-color: #2d3a4f;
+  --easy-table-scrollbar-thumb-color: #4c5d7a;;
+  --easy-table-scrollbar-corner-color: #2d3a4f;
+
+  --easy-table-loading-mask-background-color: #2d3a4f;
+}
+
+.btn-cls {
+  font-size: 30px;
+  font-weight: 400;
+  color: #c0c7d2 !important;
+}
+
+</style>
