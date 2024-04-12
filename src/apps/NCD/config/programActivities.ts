a483af8e6644e5136a59a2bd8e8027b313d0eb2d
dominickasanga <dominickasanga@gmail.com>
2024-04-12 19:28:55 +0200

    new file:   src/apps/NCD/config/programActivities.ts new file:   src/interfaces/TaskInterface.ts

---
+++ src/apps/NCD/config/programActivities.ts
@@ -0,0 +1,82 @@
+import { TaskInterface } from "@/interfaces/TaskInterface";
+
+export const PRIMARY_ACTIVITIES: TaskInterface[] = [
+  {
+    id: 'patient registration',
+    name: 'Patient registration',
+    icon: 'attributes.png'
+  },
+  {
+    id: 'vitals',
+    name: 'Vitals',
+    icon: 'vitals.png'
+  },
+  {
+    id: 'presenting complaints',
+    name: 'Presenting complaints',
+    icon: 'complaints.png'
+  },
+  {
+    id: 'hiv status',
+    name: 'HIV STatus',
+    icon: 'aids.png',
+    encounterTypeName: 'update hiv status',
+    availableOnActivitySelection: false
+  },
+  {
+    id: 'outpatient diagnosis',
+    name: 'Outpatient diagnosis',
+    icon: 'diagnosis.png'
+  },
+  {
+    id: 'outcome status',
+    name: 'Outcome status',
+    icon: 'outcomes.png',
+    availableOnActivitySelection: false
+  },
+  {
+    id: 'lab orders',
+    name: 'Lab orders',
+    icon: 'clinical-notes.png',
+    availableOnActivitySelection: false,
+    action: ({patient}: any, router: any) => {
+      router.push(`/los/forms/order/${patient.patient_id}?type=DRAW_SAMPLES`)
+    }
+  },
+  {
+    id: 'lab results',
+    name: 'Lab results',
+    icon: 'enter.png',
+    availableOnActivitySelection: false,
+    action: ({ patient }: any, router: any) => {
+      router.push({ path: `/lab/results/${patient.patient_id}`})
+    },
+  },
+  {
+    id: 'OPD social history',
+    name: 'OPD social history',
+    icon: 'medical-report.png',
+    availableOnActivitySelection: false
+  },
+  {
+    id: 'prescription',
+    name: 'Prescription',
+    encounterTypeName: 'Treatment',
+    icon: 'drugs-given.png'
+  },
+  {
+    id: 'dispensation',
+    name: 'Dispensation',
+    workflowID:"Dispensing",
+    icon: 'dispensing.png',
+    availableOnActivitySelection: false
+  },
+]
+
+export const SECONDARY_ACTIVITIES: TaskInterface[] = [
+  {
+    id: 'radiology examination',
+    name: 'radiology examination',
+    icon: 'radiology_types.png'
+  },
+]
