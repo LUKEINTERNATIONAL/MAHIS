ed7fc20125d36c2623eb604eac88b5facd1cf8d4
dominickasanga <dominickasanga@gmail.com>
2024-05-02 13:17:17 +0200

    modified:   src/views/UserManagement/addUser.vue

--- src/views/UserManagement/addUser.vue
+++ src/views/UserManagement/addUser.vue
@@ -132,6 +132,10 @@ const labels = [
     'Female',
 ]
 const sselectionListSelectedValue = ref()
+const passwordErrorMsgs = [
+    'Input must be at least 4 characters long, containing only letters, numbers, and symbols',
+    'Password does not match'
+]
 
 const props = defineProps<{
     action: any
@@ -154,38 +158,67 @@ function trigerSaveFn() {
 
 function ValidatePassword(): boolean {
     let is_valid = false
-    let error_found = false
+    let error_foundP_p1 = false
+    let error_foundP_p2 = false
+    let is_password1_valid
+    let is_password2_valid
+
+    password_input_properties[0].error_message = passwordErrorMsgs[0]
+    password_input_properties[1].error_message = passwordErrorMsgs[0]
 
     if (password_input_properties[0].dataValue.value == undefined || password_input_properties[0].dataValue.value == "") {
         password_input_properties[0].show_error.value = true
-        error_found = true
+        error_foundP_p1 = true
     }
 
     if (password_input_properties[1].dataValue.value == undefined || password_input_properties[1].dataValue.value == "") {
         password_input_properties[1].show_error.value = true
-        error_found = true
+        error_foundP_p2 = true
     }
 
-    if (error_found == true) {
-        return is_valid
-    }
 
-    const is_password1_valid = isPasswordValid(password_input_properties[0].dataValue.value)
-    const is_password2_valid = isPasswordValid(password_input_properties[1].dataValue.value)
+    if (error_foundP_p1 == false) {
+        is_password1_valid = isPasswordValid(password_input_properties[0].dataValue.value)
 
-    if (is_password1_valid == false) {
-        password_input_properties[0].show_error.value = true
-    }
+        if (is_password1_valid == false) {
+            password_input_properties[0].show_error.value = true
+            error_foundP_p1 = true
+        }
 
-    if (is_password2_valid == false) {
-        password_input_properties[1].show_error.value = true
-    }
-
-    if (is_password1_valid == true && is_password2_valid == true) {
-        if (password_input_properties[0].dataValue.value === password_input_properties[1].dataValue.value) {
+        if (is_password1_valid == true) {
             password_input_properties[0].show_error.value = false
+        }
+    }
+
+    if (error_foundP_p2 == false) {
+        is_password2_valid = isPasswordValid(password_input_properties[1].dataValue.value)
+
+        if (is_password2_valid == false) {
+            password_input_properties[1].show_error.value = true
+            error_foundP_p2 = true
+        }
+
+        if (is_password2_valid == true) {
             password_input_properties[1].show_error.value = false
-            is_valid = true
+        }
+    }
+
+    if (error_foundP_p1 == false && error_foundP_p2 == false) {
+        if (is_password1_valid == true && is_password2_valid == true) {
+            if (password_input_properties[0].dataValue.value === password_input_properties[1].dataValue.value) {
+                password_input_properties[0].show_error.value = false
+                password_input_properties[1].show_error.value = false
+                is_valid = true
+            }
+
+            if (password_input_properties[0].dataValue.value != password_input_properties[1].dataValue.value) {
+                password_input_properties[0].error_message = passwordErrorMsgs[1]
+                password_input_properties[1].error_message = passwordErrorMsgs[1]
+                password_input_properties[0].show_error.value = true
+                password_input_properties[1].show_error.value = true
+                is_valid = false
+            }
+
         }
     }
     return is_valid
@@ -227,14 +260,14 @@ const password_input_properties = [
         dataHandler: passwordInputUpDated_fn1,
         dataValue: ref(),
         show_error: ref(false),
-        error_message: 'Input required, Only letters are allowed',
+        error_message: passwordErrorMsgs[0],
     },
     {
         placeHolder: 'confirm password',
         dataHandler: passwordInputUpDated_fn2,
         dataValue: ref(),
         show_error: ref(false),
-        error_message: 'Input required, Only letters are allowed',
+        error_message: passwordErrorMsgs[0],
     },
 ]
 
