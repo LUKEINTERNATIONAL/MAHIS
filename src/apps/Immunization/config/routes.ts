2c49979525eb4beaa78fca3cd2941acc77d498c8
Senzenjani <smanjawira@gmail.com>
2024-06-28 15:33:15 +0200

    Added route file and immunization followup route

--- src/apps/Immunization/config/routes.ts
+++ src/apps/Immunization/config/routes.ts
@@ -1,1 +1,12 @@
 
+import ImmunizationFollowUpList from "../views/ImmunizationFollowupList.vue";
+
+export default [
+    {
+        path: "/ImmunizationFollowup",
+        name: "ImmunizationFollowup",
+        component: ImmunizationFollowUpList
+    }
+];
+
+ 
