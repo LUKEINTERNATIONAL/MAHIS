fb44af728cdfc50d82100154ace6cf34c797e71a
dominickasanga <dominickasanga@gmail.com>
2024-06-30 00:02:29 +0200

    modified:   src/apps/Immunization/components/Modals/nextAppointMent.vue new file:   src/apps/Immunization/services/immunization_appointment_service.ts modified:   src/apps/Immunization/services/vaccines_service.ts

---
+++ src/apps/Immunization/services/immunization_appointment_service.ts
@@ -0,0 +1,56 @@
+import { useDemographicsStore } from "@/stores/DemographicStore"
+import { Service } from "@/services/service"
+import { AppEncounterService } from "@/services/app_encounter_service";
+import { useImmunizationAppointMentStore } from "@/stores/immunizationAppointMentStore"
+import HisDate from "@/utils/Date";
+
+export class Appointment extends AppEncounterService {
+    
+    patientID: number
+    providerID: number
+
+    constructor() {
+        const patientID = Appointment.getPatientID()
+        const providerID = Appointment.getProviderID()
+        super(patientID, 7, providerID)
+        this.patientID = patientID
+        this.providerID = providerID
+    }
+
+    private static getPatientID(): number {
+        const store = useDemographicsStore();
+        const demographics = store.demographics
+        const patientID = demographics.patient_id
+        return patientID;
+    }
+
+    private static getProviderID(): number {
+        const providerID = Service.getUserID() as number
+        return providerID
+    }
+
+    async createAppointment() {
+        const _appointment_ = [] as any
+        const store = useImmunizationAppointMentStore()
+        store.selectedAppointmentMent.forEach((appointment: any) => {
+            const next_appointment_date = HisDate.toStandardHisFormat(appointment.date)
+            _appointment_.push(next_appointment_date)
+        })
+        await this.createEncounter()
+        // const res = await this.getNextAppointment();
+        // const nextAppointmentDate = res.appointment_date;
+        // const n_a_obs = await this.buildValueDate("Estimated date", nextAppointmentDate)
+        const a_obs = await this.buildValueDate("Appointment date", "mzuzu") as any
+        await this.saveObservationList([a_obs])
+
+    }
+
+    async getNextAppointment() {
+        return AppEncounterService.getJson(`/programs/${this.programID}/patients/${this.patientID}/next_appointment_date`, {date: this.date})
+    }
+      
+    static async getDailiyAppointments(date: any) {
+        const programID = AppEncounterService.getProgramID();
+        return AppEncounterService.getJson(`/programs/${programID}/booked_appointments`, {date: date, paginate: false})
+    }
+}
