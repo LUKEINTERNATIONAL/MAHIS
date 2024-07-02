79d26f5268fecb2e33d6feb10c000dcc3590b3e1
petroskayange <kayangepetros@gmail.com>
2024-07-02 06:26:57 +0200

    make screen size mixin

---
+++ src/views/Mixin/ScreenSizeMixin.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import { Service } from "@/services/service";
+import { OrderService } from "@/services/order_service";
+import { useDemographicsStore } from "@/stores/DemographicStore";
+import { mapState } from "pinia";
+export default defineComponent({
+    data: () => ({
+        screenWidth: window.innerWidth,
+        screenheight: window.innerHeight,
+    }),
+    mounted() {
+        window.addEventListener("resize", this.updateSize);
+    },
+    beforeDestroy() {
+        window.removeEventListener("resize", this.updateSize);
+    },
+    methods: {
+        updateSize() {
+            this.screenWidth = window.innerWidth;
+            this.screenheight = window.innerHeight;
+        },
+    },
+});
+</script>
