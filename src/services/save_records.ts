2ffd3df33918788f7e54fc3a94b8fbd3a51fe892
petroskayange <kayangepetros@gmail.com>
2024-07-26 06:38:35 +0200

    creating saving of all records

--- src/services/save_records.ts
+++ src/services/save_records.ts
@@ -4,37 +4,23 @@ import { useWebWorkerFn } from "@vueuse/core";
 import Localbase from "localbase";
 let db = new Localbase("db");
 export async function savePatientRecord() {
-    db.collection("patientRecords")
+    const data = await db
+        .collection("patientRecords")
         .get()
-        .then(async (patientRecord: any) => {
-            patientRecord.map(async (record: any) => {
-                const patientID = await saveDemographicsRecord(record);
-                console.log("🚀 ~ patientRecord.map ~ patientID:", patientID);
-                if (patientID) {
-                    // this.createNationID();
-                    // this.createBirthID();
-                    // if (Object.keys(this.guardianInformation[0].selectedData).length != 0) {
-                    //     if (await this.validations(this.guardianInformation, ["guardianFirstname", "guardianLastname"])) {
-                    //         this.createGuardian(patientID);
-                    //     }
-                    // }
-                    // await this.saveBirthdayData(patientID);
-                    // await this.enrollProgram(patientID);
-                    // await this.CreateRegistrationEncounter(patientID);
-                    // this.findPatient(patientID);
-                }
-            });
+        .then(async (patientRecords: any) => {
+            const patientIDs = await Promise.all(
+                patientRecords.map(async (record: any) => {
+                    let patientID = "";
+                    if (record.personInformation) {
+                        patientID = await saveDemographicsRecord(record);
+                    } else {
+                        patientID = "";
+                    }
+                    return patientID;
+                })
+            );
+            return patientIDs;
         });
+    console.log("🚀 ~ savePatientRecord ~ data:", data);
+    return data;
 }
-//  async createNationID() {
-//         if (this.validatedNationalID()) {
-//             const patient = new PatientService();
-//             await patient.updateMWNationalId(getFieldValue(this.personInformation, "nationalID", "value"));
-//         }
-//     },
-//     async createBirthID() {
-//         if (this.validatedBirthID()) {
-//             const patient = new PatientService();
-//             await patient.updateBirthId(this.birthID);
-//         }
-//     },
