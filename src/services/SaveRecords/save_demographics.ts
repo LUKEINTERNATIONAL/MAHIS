705f56e556364ef8df048c2ac80dbe55dc16f480
petroskayange <kayangepetros@gmail.com>
2024-07-26 06:39:22 +0200

    saving of demographics

--- src/services/SaveRecords/save_demographics.ts
+++ src/services/SaveRecords/save_demographics.ts
@@ -1,36 +1,65 @@
 import { PatientRegistrationService } from "@/services/patient_registration_service";
+import { PatientService } from "@/services/patient_service";
+import { modifyFieldValue, getFieldValue, getRadioSelectedValue } from "@/services/data_helpers";
+import { RelationsService } from "@/services/relations_service";
+import { Service } from "@/services/service";
+import { AppEncounterService } from "@/services/app_encounter_service";
+import { PatientProgramService } from "@/services/patient_program_service";
 
 export async function saveDemographicsRecord(record: any) {
-    const registration: any = new PatientRegistrationService();
-    await registration.registerPatient(record.personInformation, []);
-    const patientID = registration.getPersonID();
-    console.log("lllll");
-    if (patientID) {
-        return patientID;
-        // this.createNationID();
-        // this.createBirthID();
-        // if (Object.keys(this.guardianInformation[0].selectedData).length != 0) {
-        //     if (await this.validations(this.guardianInformation, ["guardianFirstname", "guardianLastname"])) {
-        //         this.createGuardian(patientID);
-        //     }
-        // }
-        // await this.saveBirthdayData(patientID);
-        // await this.enrollProgram(patientID);
-        // await this.CreateRegistrationEncounter(patientID);
-        // this.findPatient(patientID);
+    if (record.personInformation) {
+        const registration: any = new PatientRegistrationService();
+        await registration.registerPatient(record.personInformation, []);
+        const patientID = registration.getPersonID();
+        if (patientID) {
+            createNationID(record.otherPersonInformation.nationalID);
+            createBirthID(record.otherPersonInformation.birthID);
+            createGuardian(patientID, record.guardianInformation, record.otherPersonInformation.relationshipID);
+            await saveBirthdayData(patientID, record.birthRegistration);
+            await enrollProgram(patientID);
+            await CreateRegistrationEncounter(patientID);
+            return patientID;
+        } else {
+            return "";
+        }
     } else {
         return "";
     }
 }
-//  async createNationID() {
-//         if (this.validatedNationalID()) {
-//             const patient = new PatientService();
-//             await patient.updateMWNationalId(getFieldValue(this.personInformation, "nationalID", "value"));
-//         }
-//     },
-//     async createBirthID() {
-//         if (this.validatedBirthID()) {
-//             const patient = new PatientService();
-//             await patient.updateBirthId(this.birthID);
-//         }
-//     },
+async function createNationID(nationalID: any) {
+    if (nationalID) {
+        const patient = new PatientService();
+        await patient.updateMWNationalId(nationalID);
+    }
+}
+async function createBirthID(birthID: any) {
+    if (birthID) {
+        const patient = new PatientService();
+        await patient.updateBirthId(birthID);
+    }
+}
+
+async function createGuardian(patientID: any, guardianInformation: any, relationshipID: any) {
+    if (Object.keys(guardianInformation).length === 0) return;
+    const guardian: any = new PatientRegistrationService();
+    await guardian.registerGuardian(guardianInformation);
+    const guardianID = guardian.getPersonID();
+    if (relationshipID) await RelationsService.createRelation(patientID, guardianID, relationshipID);
+}
+async function saveBirthdayData(patientID: any, birthRegistration: any) {
+    if (birthRegistration.length > 0) {
+        const userID: any = Service.getUserID();
+        const registration = new AppEncounterService(patientID, 5, userID);
+        await registration.createEncounter();
+        await registration.saveObservationList(birthRegistration);
+    }
+}
+async function enrollProgram(patientId: any) {
+    const program = new PatientProgramService(patientId);
+    await program.enrollProgram();
+}
+async function CreateRegistrationEncounter(patientId: any) {
+    const encounter = new AppEncounterService(patientId, 5);
+    await encounter.createEncounter();
+    await encounter.saveValueCodedObs("Type of patient", "New Patient");
+}
