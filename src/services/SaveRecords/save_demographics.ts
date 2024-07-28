8a4e71bc4bd03c8329ccdbc7475256e463fe400b
petroskayange <kayangepetros@gmail.com>
2024-07-28 08:23:59 +0200

    improving saving of demographics

--- src/services/SaveRecords/save_demographics.ts
+++ src/services/SaveRecords/save_demographics.ts
@@ -7,128 +7,106 @@ import { AppEncounterService } from "@/services/app_encounter_service";
 import { PatientProgramService } from "@/services/patient_program_service";
 import Localbase from "localbase";
 import { toastDanger, toastSuccess, toastWarning } from "@/utils/Alerts";
-let db = new Localbase("db");
+
+const db = new Localbase("db");
+
 export async function saveDemographicsRecord(record: any) {
-    if ((await validateNationalID(record.otherPersonInformation.nationalID)) && (await validateBirthID(record.otherPersonInformation.birthID))) {
-        let patientID = "";
-        if (record.personInformation && record.saveStatusPersonInformation == "pending") {
-            try {
-                const registration: any = new PatientRegistrationService();
-                await registration.registerPatient(record.personInformation, []);
-                patientID = registration.getPersonID();
-                await updateSaveStatus(record, {
-                    saveStatusPersonInformation: "complete",
-                    serverPatientID: patientID,
-                });
-            } catch (error) {
-                toastDanger("Failing to save person information");
-            }
-        }
-        patientID = patientID || record.serverPatientID;
-        if (patientID) {
-            createNationID(record.otherPersonInformation.nationalID);
-            createBirthID(record.otherPersonInformation.birthID);
-            createGuardian(patientID, record);
-            await saveBirthdayData(patientID, record);
-            await enrollProgram(patientID);
-            await CreateRegistrationEncounter(patientID);
-        }
-    }
+    if (!(await validateID(record.otherPersonInformation))) return;
+
+    const patientID = await savePersonInformation(record);
+    if (!patientID) return;
+
+    await Promise.all([createGuardian(patientID, record), saveBirthdayData(patientID, record)]);
 }
-async function createNationID(nationalID: any) {
-    if (nationalID) {
-        const patient = new PatientService();
-        await patient.updateMWNationalId(nationalID);
-    }
+
+async function validateID({ nationalID, birthID }: any) {
+    return (await validateNationalID(nationalID)) && (await validateBirthID(birthID));
 }
-async function createBirthID(birthID: any) {
-    if (birthID) {
-        const patient = new PatientService();
-        await patient.updateBirthId(birthID);
+
+async function savePersonInformation(record: any) {
+    if (record.personInformation && record.saveStatusPersonInformation === "pending") {
+        try {
+            const registration = new PatientRegistrationService();
+            await registration.registerPatient(record.personInformation, []);
+            const patientID = registration.getPersonID();
+            await updateSaveStatus(record, {
+                saveStatusPersonInformation: "complete",
+                serverPatientID: patientID,
+            });
+            createIDs(record.otherPersonInformation);
+            enrollProgram(patientID);
+            createRegistrationEncounter(patientID);
+            return patientID;
+        } catch (error) {
+            toastDanger("Failed to save person information");
+        }
     }
+    return record.serverPatientID;
+}
+
+async function createIDs({ nationalID, birthID }: any) {
+    const patient = new PatientService();
+    if (nationalID) await patient.updateMWNationalId(nationalID);
+    if (birthID) await patient.updateBirthId(birthID);
 }
 
 async function createGuardian(patientID: any, record: any) {
-    if (record.saveStatusGuardianInformation == "pending") {
-        if (Object.keys(record.guardianInformation).length > 0) {
+    if (record.saveStatusGuardianInformation === "pending") {
+        if (record.guardianInformation.given_name && record.guardianInformation.family_name && record.otherPersonInformation.relationshipID) {
             try {
-                const guardian: any = new PatientRegistrationService();
+                const guardian = new PatientRegistrationService();
                 await guardian.registerGuardian(record.guardianInformation);
                 const guardianID = guardian.getPersonID();
-                if (record.otherPersonInformation.relationshipID)
-                    await RelationsService.createRelation(patientID, guardianID, record.otherPersonInformation.relationshipID);
-
-                await updateSaveStatus(record, {
-                    saveStatusGuardianInformation: "complete",
-                });
+                await RelationsService.createRelation(patientID, guardianID, record.otherPersonInformation.relationshipID);
             } catch (error) {
-                toastDanger("Failing to save guarding information");
+                toastDanger("Failed to save guardian information");
             }
         }
-        if (Object.keys(record.guardianInformation).length == 0) {
-            await updateSaveStatus(record, {
-                saveStatusGuardianInformation: "complete",
-            });
-        }
+        await updateSaveStatus(record, { saveStatusGuardianInformation: "complete" });
     }
 }
+
 async function saveBirthdayData(patientID: any, record: any) {
-    if (record.saveStatusBirthRegistration == "pending") {
+    if (record.saveStatusBirthRegistration === "pending" && record.birthRegistration.length > 0) {
         try {
-            if (record.birthRegistration.length > 0) {
-                const userID: any = Service.getUserID();
-                const registration = new AppEncounterService(patientID, 5, userID);
-                await registration.createEncounter();
-                await registration.saveObservationList(record.birthRegistration);
-                await updateSaveStatus(record, {
-                    saveStatusBirthRegistration: "complete",
-                });
-            }
+            const userID: any = Service.getUserID();
+            const registration = new AppEncounterService(patientID, 5, userID);
+            await registration.createEncounter();
+            await registration.saveObservationList(record.birthRegistration);
         } catch (error) {
-            toastDanger("Failing to save birth information");
+            toastDanger("Failed to save birth information");
         }
-    }
-    if (record.birthRegistration.length == 0) {
-        await updateSaveStatus(record, {
-            saveStatusBirthRegistration: "complete",
-        });
+        await updateSaveStatus(record, { saveStatusBirthRegistration: "complete" });
     }
 }
+
 async function enrollProgram(patientId: any) {
     const program = new PatientProgramService(patientId);
     await program.enrollProgram();
 }
-async function CreateRegistrationEncounter(patientId: any) {
+
+async function createRegistrationEncounter(patientId: any) {
     const encounter = new AppEncounterService(patientId, 5);
     await encounter.createEncounter();
     await encounter.saveValueCodedObs("Type of patient", "New Patient");
 }
+
 async function updateSaveStatus(record: any, saveStatus: any) {
     await db.collection("patientRecords").doc({ offlinePatientID: record.offlinePatientID }).update(saveStatus);
 }
 
 async function validateNationalID(nationalID: any) {
-    if (nationalID != "") {
-        if (await checkIDExistences(28, nationalID)) {
-            toastWarning("The national ID is already assigned to another person");
-            return false;
-        }
-    }
-    return true;
+    return (
+        nationalID === "" ||
+        !(await checkIDExistence(28, nationalID)) ||
+        (toastWarning("The national ID is already assigned to another person"), false)
+    );
 }
 
 async function validateBirthID(birthID: any) {
-    if (birthID != "") {
-        if (await checkIDExistences(23, birthID)) {
-            toastWarning("The Birth ID is already assigned to another person");
-            return false;
-        }
-    }
-    return true;
+    return birthID === "" || !(await checkIDExistence(23, birthID)) || (toastWarning("The Birth ID is already assigned to another person"), false);
 }
-async function checkIDExistences(nid: any, identifierId: any) {
-    if (!nid) return false;
-    const people = await PatientService.findByOtherID(identifierId, nid);
-    if (people.length > 0) return true;
-    else return false;
+
+async function checkIDExistence(nid: any, identifierId: any) {
+    return nid ? (await PatientService.findByOtherID(identifierId, nid)).length > 0 : false;
 }
