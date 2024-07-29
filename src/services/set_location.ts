34ee1936cfc171c347f990b3247e1c1f0f2b1a7b
petroskayange <kayangepetros@gmail.com>
2024-07-29 11:16:17 +0200

    adding offline location

--- src/services/set_location.ts
+++ src/services/set_location.ts
@@ -4,52 +4,31 @@ export async function setOfflineData() {
     setLocation();
 }
 export async function setLocation() {
-    let locationData = await db
+    return await db
         .collection("location")
         .get()
-        .then(async (location: any) => {
-            if (location) {
-                await db
-                    .collection("location")
-                    .doc({ id: "location" })
-                    .update({
-                        id: "location",
-                        villages: await getVillages(location),
-                        districts: await getDistricts(location),
-                        TAs: await getTAs(location),
-                    });
+        .then(async (locationData: any) => {
+            let location = locationData[0];
+            if (locationData?.length > 0) {
+                return location;
             } else {
-                await db.collection("location").add({
+                const newData = await db.collection("location").add({
                     id: "location",
                     villages: await getVillages(location),
                     districts: await getDistricts(location),
                     TAs: await getTAs(location),
                 });
+                return newData?.data?.data;
             }
         });
-
-    locationData = await db.collection("location").get()[0];
 }
 
 export async function getDistricts(location: any) {
-    if (!location?.districts || location?.districts?.length == 0) {
-        return await LocationService.getAllDistricts();
-    } else {
-        return location?.districts;
-    }
+    return await LocationService.getAllDistricts();
 }
 export async function getTAs(location: any) {
-    if (!location?.TAs || location?.TAs?.length == 0) {
-        return await LocationService.getAllTraditionalAuthorities();
-    } else {
-        return location?.TAs;
-    }
+    return await LocationService.getAllTraditionalAuthorities();
 }
 export async function getVillages(location: any) {
-    if (!location?.villages || location?.villages?.length == 0) {
-        console.log("🚀 ~ llllllllllllllllllgetVillages ~ location:", location);
-        return await LocationService.getAllVillages();
-    } else {
-        return location?.villages;
-    }
+    return await LocationService.getAllVillages();
 }
