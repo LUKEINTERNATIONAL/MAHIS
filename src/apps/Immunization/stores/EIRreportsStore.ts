c585b047cab48ab02272d2ed716ee6805ebc58f8
dominickasanga <dominickasanga@gmail.com>
2024-08-12 13:56:56 +0200

    modified:   src/apps/Immunization/stores/EIRreportsStore.ts

--- src/apps/Immunization/stores/EIRreportsStore.ts
+++ src/apps/Immunization/stores/EIRreportsStore.ts
@@ -3,10 +3,14 @@ import { defineStore } from "pinia";
 export const useChangeStatusStore = defineStore("ChangeStatusStore", {
     state: () => ({
         navigationPayload: {},
+        previousRoute: '',
     }),
     actions: {
         setNavigationPayload(payload: any): void{
             this.navigationPayload = payload
+        },
+        setPreviousRoute(route: string): void {
+            this.previousRoute = route
         }
     },
     persist: true,
