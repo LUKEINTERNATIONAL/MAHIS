0c9738a744fbaacc60efe1b8f13da4f32dcdace2
Mwayanjana Bolokonya <mwayab@gmail.com>
2024-08-21 13:18:52 +0200

    initial commit

---
+++ src/components/PrivacyPolicy.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="pdf-viewer">
+      <iframe
+        v-if="pdfUrl"
+        :src="pdfUrl"
+        type="application/pdf"
+        width="100%"
+        height="100%"
+        frameborder="0"
+      ></iframe>
+      <p v-else>Loading...</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "PrivacyPolicy",
+    data() {
+      return {
+        pdfUrl: null,
+      };
+    },
+    mounted() {
+      this.pdfUrl = "../../public/assets/MaHIS_Privacy_Policy.pdf";
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .pdf-viewer {
+    width: 100%;
+    height: 100vh;
+    overflow: hidden;
+  }
+  </style>
---
+++ src/views/PrivacyPolicyView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <h1>Privacy Policy</h1>
+      <PrivacyPolicy />
+    </div>
+  </template>
+  
+  <script>
+  import PrivacyPolicy from "@/components/PrivacyPolicy.vue";
+  
+  export default {
+    components: {
+      PrivacyPolicy,
+    },
+  };
+  </script>
