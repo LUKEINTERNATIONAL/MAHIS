dba0321f3f7426873251e336e6cccccea7790fd7
petroskayange <kayangepetros@gmail.com>
2024-08-22 16:10:33 +0200

    DDE global property

--- src/stores/GlobalPropertyStore.ts
+++ src/stores/GlobalPropertyStore.ts
@@ -1,17 +1,21 @@
-import { defineStore } from 'pinia'
+import { defineStore } from "pinia";
 import { Service } from "@/services/service";
 
-export const useGlobalPropertyStore = defineStore('globalPropertyStore',{
+export const useGlobalPropertyStore = defineStore("globalPropertyStore", {
     state: () => ({
         globalPropertyStore: {
-            'sitePrefix':  loadSitePrefix()
-        }
+            sitePrefix: loadSitePrefix(),
+            dde_enabled: loadDDEStatus(),
+        },
     }),
-    persist:true,
-
-})
+    persist: true,
+});
 
 async function loadSitePrefix() {
-    const req = await Service.getJson('global_properties', { property: 'site_prefix' });
-    return req['site_prefix']
-}   
+    const req = await Service.getJson("global_properties", { property: "site_prefix" });
+    return req["site_prefix"];
+}
+async function loadDDEStatus() {
+    const req = await Service.getJson("global_properties", { property: "dde_enabled" });
+    return req["dde_enabled"];
+}
