b3fc6cde5ac79cf473282f21aa54b1e8a7c17dd5
petroskayange <kayangepetros@gmail.com>
2024-08-23 05:13:28 +0200

    redesigning person matching

--- src/components/PersonMatchView.vue
+++ src/components/PersonMatchView.vue
@@ -1,52 +1,91 @@
 <template>
-    <ion-row>
-        <ion-col size="5">
-            <div class="large-card">
-                <h3>Matches found: ({{ deduplicationData.length }}):</h3>
-                <ion-list>
-                    <ion-item
-                        button
-                        v-for="(result, index) in deduplicationData"
-                        :key="index"
-                        :detail="true"
-                        :color="result.value === selectedResult.value ? 'light' : ''"
-                        @click="onSelect(result)"
-                    >
-                        <ion-avatar>
-                            <ion-img src="/assets/images/avatar.svg" />
-                        </ion-avatar>
-                        <ion-label>
-                            {{ result?.person?.given_name }} {{ result?.person?.family_name }}<br />
-                            Home District: <b>{{ result?.person?.home_district }}</b> <br />
-                            Home TA: <b>{{ result?.person?.home_traditional_authority }}</b> Score: <b> {{ result?.score || "-" }} </b>
-                        </ion-label>
-                    </ion-item>
-                </ion-list>
+    <!-- Spinner -->
+    <div v-if="isLoading" class="spinner-overlay">
+        <ion-spinner name="bubbles"></ion-spinner>
+        <div class="loading-text">Please wait...</div>
+    </div>
+    <ion-header>
+        <div class="header position_content">
+            <div style="display: flex; align-items: center" @click="dismiss()">
+                <ion-icon slot="separator" size="large" :icon="iconsContent.arrowLeft"></ion-icon>
             </div>
-        </ion-col>
-        <ion-col size="7">
-            <div class="large-card">
-                <h1>Match score: {{ selectedResult?.score || "-" }}</h1>
-                <ion-list>
-                    <ion-item>
-                        {{ selectedResult }}
-                        <ion-label color="danger">
-                            <b>{{ item }}</b>
-                        </ion-label>
-                    </ion-item>
-                </ion-list>
-            </div>
-        </ion-col>
-    </ion-row>
+            <div style="font-size: 1.2em; font-weight: 700">Matches found: ({{ deduplicationData.length }})</div>
+        </div>
+    </ion-header>
+    <ion-content :fullscreen="true" class="ion-padding" style="--background: #fff">
+        <ion-accordion-group ref="accordionGroup" class="previousView" value="0">
+            <ion-accordion
+                style="margin-bottom: 15px"
+                :value="index"
+                toggle-icon-slot="start"
+                class="custom_card"
+                button
+                v-for="(result, index) in deduplicationData"
+                :key="index"
+                :detail="true"
+                :color="result.value === selectedResult.value ? 'light' : ''"
+                @click="onSelect(result)"
+            >
+                <ion-item slot="header">
+                    <ion-label class="previousLabel"> {{ result?.person?.given_name }} {{ result?.person?.family_name }}</ion-label>
+                    <div class="due_date">
+                        Score: <b> {{ result?.score || "-" }} </b>
+                    </div>
+                </ion-item>
+                <div class="ion-padding" slot="content">
+                    <ion-row>
+                        <ion-col><b>To be Registered</b></ion-col>
+                        <ion-col><b>Already Registered</b></ion-col>
+                    </ion-row>
+                    <ion-row>
+                        <ion-col>
+                            <ion-row>
+                                Name: <b>{{ result?.person?.given_name }} {{ result?.person?.family_name }}</b></ion-row
+                            >
+                            <ion-row
+                                >Gender: <b>{{ result?.person?.gender }}</b></ion-row
+                            >
+                            <ion-row
+                                >Birthdate: <b>{{ result?.person?.birthdate }}</b></ion-row
+                            >
+                            <ion-row
+                                >Home District: <b>{{ result?.person?.home_district }}</b></ion-row
+                            >
+                            <ion-row
+                                >Home TA: <b>{{ result?.person?.home_traditional_authority }}</b></ion-row
+                            >
+                        </ion-col>
+                        <ion-col>
+                            <ion-row>
+                                Name: <b>{{ result?.person?.given_name }} {{ result?.person?.family_name }}</b></ion-row
+                            >
+                            <ion-row
+                                >Gender: <b>{{ result?.person?.gender }}</b></ion-row
+                            >
+                            <ion-row
+                                >Birthdate: <b>{{ result?.person?.birthdate }}</b></ion-row
+                            >
+                            <ion-row
+                                >Home District: <b>{{ result?.person?.home_district }}</b></ion-row
+                            >
+                            <ion-row
+                                >Home TA: <b>{{ result?.person?.home_traditional_authority }}</b></ion-row
+                            >
+                        </ion-col>
+                    </ion-row>
+                </div>
+            </ion-accordion>
+        </ion-accordion-group>
+    </ion-content>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
-import { IonRow, IonLabel, IonAvatar, IonCol, IonImg, IonList, IonItem } from "@ionic/vue";
+import { IonRow, IonLabel, IonAvatar, IonCol, IonImg, IonList, IonItem, modalController, IonIcon } from "@ionic/vue";
 import FieldMixinVue from "@/components/FormElements/FieldMixin.vue";
 import { Option } from "@/components/Forms/FieldInterface";
+import { icons } from "@/utils/svg";
 
 export default defineComponent({
-    mixins: [FieldMixinVue],
     components: {
         IonLabel,
         IonAvatar,
@@ -55,11 +94,16 @@ export default defineComponent({
         IonCol,
         IonList,
         IonItem,
+        IonIcon,
     },
     data: () => ({
         selectedResult: {} as any,
+        iconsContent: icons,
     }),
     methods: {
+        dismiss() {
+            modalController.dismiss();
+        },
         async init() {
             this.$emit("onFieldActivated", this);
             this.onSelect(this.deduplicationData[0] || {});
@@ -103,5 +147,23 @@ export default defineComponent({
     -webkit-box-shadow: 0px -2px 19px -2px rgba(196, 190, 196, 1);
     -moz-box-shadow: 0px -2px 19px -2px rgba(196, 190, 196, 1);
     box-shadow: 0px -2px 19px -2px rgba(196, 190, 196, 1);
+}
+.header {
+    display: flex;
+    gap: 20px;
+    padding: 10px 20px 10px 20px;
+}
+.position_content {
+    max-width: 100vw;
+}
+.accordion-expanded {
+    background-color: #ddeedd;
+}
+button[aria-expanded="false"] {
+    background: rgba(var(--ion-color-rose-rgb), 0.25);
+}
+ion-accordion.accordion-expanding ion-item[slot="header"],
+ion-accordion.accordion-expanded ion-item[slot="header"] {
+    --background: #ddeedd;
 }
 </style>
