3575029e1d485dbb09a20a19305582782875c0a4
petroskayange <kayangepetros@gmail.com>
2024-08-23 07:36:07 +0200

    improving person matching view

--- src/components/PersonMatchView.vue
+++ src/components/PersonMatchView.vue
@@ -6,14 +6,14 @@
     </div>
     <ion-header>
         <div class="header position_content">
-            <div style="display: flex; align-items: center" @click="dismiss()">
+            <div style="display: flex; align-items: center" @click="dismiss('back')">
                 <ion-icon slot="separator" size="large" :icon="iconsContent.arrowLeft"></ion-icon>
             </div>
             <div style="font-size: 1.2em; font-weight: 700">Matches found: ({{ deduplicationData.length }})</div>
         </div>
     </ion-header>
     <ion-content :fullscreen="true" class="ion-padding" style="--background: #fff">
-        <ion-accordion-group ref="accordionGroup" class="previousView" value="0">
+        <ion-accordion-group ref="accordionGroup" class="previousView" value="0" @ionChange="updateAccordionState">
             <ion-accordion
                 style="margin-bottom: 15px"
                 :value="index"
@@ -24,12 +24,12 @@
                 :key="index"
                 :detail="true"
                 :color="result.value === selectedResult.value ? 'light' : ''"
-                @click="onSelect(result)"
+                @click="setData(result)"
             >
                 <ion-item slot="header">
                     <ion-label class="previousLabel"> {{ result?.person?.given_name }} {{ result?.person?.family_name }}</ion-label>
                     <div class="due_date">
-                        Score: <b> {{ result?.score || "-" }} </b>
+                        Score: <b> {{ probabilityToPercentage(result?.score) || "-" }} </b>
                     </div>
                 </ion-item>
                 <div class="ion-padding" slot="content">
@@ -40,19 +40,19 @@
                     <ion-row>
                         <ion-col>
                             <ion-row>
-                                Name: <b>{{ result?.person?.given_name }} {{ result?.person?.family_name }}</b></ion-row
+                                Name: <b>{{ to_be_registered?.given_name }} {{ to_be_registered?.family_name }}</b></ion-row
                             >
                             <ion-row
-                                >Gender: <b>{{ result?.person?.gender }}</b></ion-row
+                                >Gender: <b>{{ to_be_registered?.gender }}</b></ion-row
                             >
                             <ion-row
-                                >Birthdate: <b>{{ result?.person?.birthdate }}</b></ion-row
+                                >Birthdate: <b>{{ to_be_registered?.birthdate }}</b></ion-row
                             >
                             <ion-row
-                                >Home District: <b>{{ result?.person?.home_district }}</b></ion-row
+                                >Home District: <b>{{ to_be_registered?.home_district }}</b></ion-row
                             >
                             <ion-row
-                                >Home TA: <b>{{ result?.person?.home_traditional_authority }}</b></ion-row
+                                >Home TA: <b>{{ to_be_registered?.home_traditional_authority }}</b></ion-row
                             >
                         </ion-col>
                         <ion-col>
@@ -63,7 +63,7 @@
                                 >Gender: <b>{{ result?.person?.gender }}</b></ion-row
                             >
                             <ion-row
-                                >Birthdate: <b>{{ result?.person?.birthdate }}</b></ion-row
+                                >Birthdate: <b>{{ dateFormat(result?.person?.birthdate) }}</b></ion-row
                             >
                             <ion-row
                                 >Home District: <b>{{ result?.person?.home_district }}</b></ion-row
@@ -77,13 +77,20 @@
             </ion-accordion>
         </ion-accordion-group>
     </ion-content>
+    <ion-footer collapse="fade" class="ion-no-border">
+        <div style="display: flex; justify-content: end; gap: 10px">
+            <ion-button fill="solid" color="secondary" @click="dismiss('dismiss')"> Not Duplicate </ion-button>
+            <ion-button @click="onSelect()" fill="solid" :disabled="!isAnyAccordionOpen">Select </ion-button>
+        </div>
+    </ion-footer>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
-import { IonRow, IonLabel, IonAvatar, IonCol, IonImg, IonList, IonItem, modalController, IonIcon } from "@ionic/vue";
+import { IonRow, IonLabel, IonAvatar, IonCol, IonImg, IonList, IonItem, modalController, IonIcon, IonButton } from "@ionic/vue";
 import FieldMixinVue from "@/components/FormElements/FieldMixin.vue";
 import { Option } from "@/components/Forms/FieldInterface";
 import { icons } from "@/utils/svg";
+import HisDate from "@/utils/Date";
 
 export default defineComponent({
     components: {
@@ -95,35 +102,42 @@ export default defineComponent({
         IonList,
         IonItem,
         IonIcon,
+        IonButton,
     },
     data: () => ({
         selectedResult: {} as any,
         iconsContent: icons,
+        isAnyAccordionOpen: true,
     }),
     methods: {
-        dismiss() {
-            modalController.dismiss();
+        updateAccordionState(event: CustomEvent) {
+            const accordionGroup = this.$refs.accordionGroup as HTMLIonAccordionGroupElement;
+            this.isAnyAccordionOpen = accordionGroup.value !== undefined;
+        },
+        dismiss(dismiss: any) {
+            modalController.dismiss(dismiss);
         },
         async init() {
-            this.$emit("onFieldActivated", this);
-            this.onSelect(this.deduplicationData[0] || {});
+            this.setData(this.deduplicationData[0] || {});
         },
-        async onSelect(item: any) {
-            if (!item) {
-                return;
-            }
-            if (this.onValue) {
-                const ok = await this.onValue(item, this);
-                if (!ok) {
-                    return;
-                }
-            }
-            this.selectedResult = item;
-            this.$emit("onValue", item);
+        async onSelect() {
+            modalController.dismiss(this.selectedResult);
+        },
+        setData(data: any) {
+            this.selectedResult = data;
+        },
+        dateFormat(date: any) {
+            return HisDate.toStandardHisDisplayFormat(date);
+        },
+        probabilityToPercentage(probability: any) {
+            return (probability * 100).toFixed(2) + "%";
         },
     },
     props: {
         deduplicationData: {
+            default: [] as any,
+        },
+        to_be_registered: {
             default: [] as any,
         },
     },
