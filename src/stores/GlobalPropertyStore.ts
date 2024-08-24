d67a2402e2124cb1b2815a15c9fd09faedecfa4e
petroskayange <kayangepetros@gmail.com>
2024-08-24 18:33:58 +0200

    setting up state management for DDE

--- src/stores/GlobalPropertyStore.ts
+++ src/stores/GlobalPropertyStore.ts
@@ -4,18 +4,30 @@ import { Service } from "@/services/service";
 export const useGlobalPropertyStore = defineStore("globalPropertyStore", {
     state: () => ({
         globalPropertyStore: {
-            sitePrefix: loadSitePrefix(),
-            dde_enabled: loadDDEStatus(),
+            sitePrefix: false,
+            dde_enabled: false,
         },
     }),
+    actions: {
+        async loadSitePrefix() {
+            const req = await Service.getJson("global_properties", { property: "site_prefix" });
+            this.globalPropertyStore.sitePrefix = req["site_prefix"];
+        },
+        async loadDDEStatus() {
+            const req = await Service.getJson("global_properties", { property: "dde_enabled" });
+            this.globalPropertyStore.dde_enabled = req["dde_enabled"];
+        },
+        async loadGlobalProperty() {
+            await this.loadSitePrefix();
+            await this.loadDDEStatus();
+        },
+        async setGlobalProperty(prop: any, val: any) {
+            await Service.postJson("global_properties", {
+                property: prop,
+                property_value: val.toString(),
+            });
+            await this.loadGlobalProperty();
+        },
+    },
     persist: true,
 });
-
-async function loadSitePrefix() {
-    const req = await Service.getJson("global_properties", { property: "site_prefix" });
-    return req["site_prefix"];
-}
-async function loadDDEStatus() {
-    const req = await Service.getJson("global_properties", { property: "dde_enabled" });
-    return req["dde_enabled"];
-}
