30808df171d642fc3e3ff1fc845a705c5e14b7f5
Wisdom Changala <wisdomchangala@gmail.com>
2024-08-30 09:29:46 +0200

    Past obstretic history alerts

--- src/apps/ANC/store/profile/PastObstreticHistoryStore.ts
+++ src/apps/ANC/store/profile/PastObstreticHistoryStore.ts
@@ -27,7 +27,19 @@ export const useObstreticHistoryStore = defineStore("obstreticHistoryStore", {
             },
           ],
         },
+        alerts: [
+          {
+            backgroundColor: "",
+            status: "",
+            icon: "",
+            textColor: "",
+            value: "",
+            name: "",
+            index: "",
+          },
+        ],
       },
+
       {
         isFinishBtn: false,
         sectionHeader: "",
@@ -107,7 +119,7 @@ export const useObstreticHistoryStore = defineStore("obstreticHistoryStore", {
             title: "Was last live birth preterm?",
             selectedValue: "",
             name: "Was last live birth preterm?",
-            // displayNone: true,
+            displayNone: false,
           },
           data: [
             {
@@ -132,10 +144,10 @@ export const useObstreticHistoryStore = defineStore("obstreticHistoryStore", {
         radioBtnContent: {
           header: {
             class: "bold",
-            title: "Was last live birth was full term full term (8 to 34 weeks)",
+            title: "Was last live birth full term (8 to 34 weeks)?",
             selectedValue: "",
             name: "Last live birth preterm was full term",
-            // displayNone: true,
+            displayNone: false,
           },
           data: [
             {
@@ -163,7 +175,7 @@ export const useObstreticHistoryStore = defineStore("obstreticHistoryStore", {
             title: "Last live birth had congenital abnormalities??",
             selectedValue: "",
             name: "Last live birth had congenital abnormalities",
-            // displayNone: true,
+            displayNone: false,
           },
           data: [
             {
@@ -181,46 +193,6 @@ export const useObstreticHistoryStore = defineStore("obstreticHistoryStore", {
           ],
         },
       },
-    ] as any,
-    abnormalities: [
-      {
-        selectdData: [],
-        isFinishBtn: false,
-        classDash: "dashed_bottom_border _padding",
-        radioBtnContent: {
-          header: {
-            title: "Last live birth had congenital abnormalities?",
-            selectedValue: "",
-          },
-          data: [
-            {
-              name: "Yes",
-              value: "Yes",
-              labelPlacement: "start",
-              colSize: "7",
-              justify: "space-between",
-            },
-            {
-              name: "No",
-              value: "No",
-              labelPlacement: "start",
-              colSize: "7",
-              justify: "space-between",
-            },
-            {
-              name: "Dont know",
-              value: "Dont know",
-              labelPlacement: "start",
-              colSize: "7",
-              justify: "space-between",
-            },
-          ],
-        },
-      },
-    ],
-    preterm: [],
-    modeOfDelivery: [] as any,
-    Complications: [
       {
         selectdData: [],
         classDash: "dashed_bottom_border",
@@ -228,9 +200,10 @@ export const useObstreticHistoryStore = defineStore("obstreticHistoryStore", {
           header: {
             class: "bold",
             title:
-              "Did the woman have any of the complications during the past pregnancies?",
+                "Did the woman have any of the complications during the past pregnancies?",
             selectedValue: "",
             name: "past pregnancies complications",
+            displayNone:false
           },
           data: [
             {
@@ -260,6 +233,8 @@ export const useObstreticHistoryStore = defineStore("obstreticHistoryStore", {
             title: "",
             selectedValue: "",
             name: "past pregnancies complications",
+            displayNone:false
+
           },
           data: [
             {
@@ -289,6 +264,8 @@ export const useObstreticHistoryStore = defineStore("obstreticHistoryStore", {
             title: "",
             selectedValue: "",
             name: "past pregnancies complications",
+            displayNone:false
+
           },
           data: [
             {
@@ -310,26 +287,6 @@ export const useObstreticHistoryStore = defineStore("obstreticHistoryStore", {
           ],
         },
       },
-      // {
-      //   classDash: "dashed_bottom_border",
-      //   checkboxBtnContent: {
-      //     header: {
-      //       title: "",
-      //       selectedValue: "",
-      //       name: "past pregnancies complications",
-      //     },
-      //     data: [
-      //       // {
-      //       //   name: "Forceps",
-      //       //   value: "forceps",
-      //       //   checked: false,
-      //       //   labelPlacement: "start",
-      //       //   colSize: "6",
-      //       //   justify: "space-between",
-      //       // },
-      //     ],
-      //   },
-      // },
 
       {
         classDash: "dashed_bottom_border",
@@ -338,6 +295,8 @@ export const useObstreticHistoryStore = defineStore("obstreticHistoryStore", {
             title: "",
             selectedValue: "",
             name: "past pregnancies complications",
+            displayNone:false
+
           },
           data: [
             {
@@ -367,6 +326,8 @@ export const useObstreticHistoryStore = defineStore("obstreticHistoryStore", {
             title: "",
             selectedValue: "",
             name: "past pregnancies complications",
+            displayNone:false
+
           },
           data: [
             {
@@ -395,6 +356,8 @@ export const useObstreticHistoryStore = defineStore("obstreticHistoryStore", {
             title: "",
             selectedValue: "",
             name: "past pregnancies complications",
+            displayNone:false
+
           },
           data: [
             {
@@ -442,6 +405,47 @@ export const useObstreticHistoryStore = defineStore("obstreticHistoryStore", {
           ],
         },
       },
+    ] as any,
+    abnormalities: [
+      {
+        selectdData: [],
+        isFinishBtn: false,
+        classDash: "dashed_bottom_border _padding",
+        radioBtnContent: {
+          header: {
+            title: "Last live birth had congenital abnormalities?",
+            selectedValue: "",
+          },
+          data: [
+            {
+              name: "Yes",
+              value: "Yes",
+              labelPlacement: "start",
+              colSize: "7",
+              justify: "space-between",
+            },
+            {
+              name: "No",
+              value: "No",
+              labelPlacement: "start",
+              colSize: "7",
+              justify: "space-between",
+            },
+            {
+              name: "Dont know",
+              value: "Dont know",
+              labelPlacement: "start",
+              colSize: "7",
+              justify: "space-between",
+            },
+          ],
+        },
+      },
+    ],
+    preterm: [],
+    modeOfDelivery: [] as any,
+    Complications: [
+
     ] as any,
   }),
   actions: {
