0aa0cee15ac9b783128524f4f64c69181c2286fd
Senzenjani <smanjawira@gmail.com>
2024-09-03 16:56:32 +0200

    Changed wording to missed doses

--- src/views/ImmunizationOverDueReport.vue
+++ src/views/ImmunizationOverDueReport.vue
@@ -29,7 +29,7 @@
                         <tr>
                             <th>Name</th>
                             <th>Date Of Birth</th>
-                            <th>Missed Visits</th>
+                            <th>Missed Doses</th>
                             <th>Action</th>
                         </tr>
                     </thead>
