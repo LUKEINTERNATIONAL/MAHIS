175be252e720e134934d2fcf4dc3dc6c6eb832cd
petroskayange <kayangepetros@gmail.com>
2024-09-13 09:19:10 +0200

    offline menu

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -28,6 +28,11 @@
                         <ion-label class="header">Inventory Management </ion-label>
                     </ion-item>
                 </ion-accordion>
+                <ion-accordion value="second" @click="navigationMenu('OfflineRecords')" toggle-icon="">
+                    <ion-item slot="header" color="light">
+                        <ion-label class="header">Manage Offline Records </ion-label>
+                    </ion-item>
+                </ion-accordion>
                 <ion-accordion value="fourth">
                     <ion-item slot="header" color="light">
                         <ion-label class="header">Reports</ion-label>
@@ -74,11 +79,7 @@
                                 EPI Monthly Report
                             </ion-item>
 
-                            <ion-item
-                                @click="navigationMenu('OverDueReport')"
-                                class="list-content"
-                                style="cursor: pointer"
-                            >
+                            <ion-item @click="navigationMenu('OverDueReport')" class="list-content" style="cursor: pointer">
                                 EIR Overdue Report
                             </ion-item>
 
