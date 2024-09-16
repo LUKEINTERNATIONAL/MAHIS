c7e247725d1241d71c1d11208f7682d62c2cf53e
Louis Nyirongo <louis@Louiss-MacBook-Pro.local>
2024-09-16 11:51:10 +0200

    spefiy congenital abnormalities

--- src/apps/LABOUR/components/delivery details/BabyDetailsModal.vue
+++ src/apps/LABOUR/components/delivery details/BabyDetailsModal.vue
@@ -1,44 +1,29 @@
 <template>
-    <ion-modal  :is-open="isOpen" :show-backdrop="true" @didDismiss="closeModal">
-     <ion-content>
-        <ion-title style="color:black">{{title}}</ion-title>
-         <basic-form
-            :contentData="babyDetails"
-            :initialData="initialData"
-             @update:selected="handleInputData" 
-             @update:inputValue="handleInputData"
-            ></basic-form>
-        <div style="display:flex;">
-          <DynamicButton
-              expand="block"
-              @click="confirm()"
-              name="Save"
-              :style="`flex:1`"
-          />
-          <DynamicButton
-              expand="block"
-              @click="onNo()"
-              :style="`flex:1`"
-              name="Cancel"
-              fill="clear"
-          />
-        </div>
-      </ion-content>
-    </ion-modal>
-  </template>
-  
-  <script lang="ts">
-  import { defineComponent, PropType } from 'vue';
-  import {
-    IonModal,
-    IonHeader,
-    IonToolbar,
-    IonTitle,
-    IonButtons,
-    IonButton,
-    IonContent,
-    IonIcon
-  } from '@ionic/vue';
+  <ion-modal :is-open="isOpen" :show-backdrop="true" @didDismiss="closeModal">
+    <ion-content>
+      <ion-title style="color:black">{{ title }}</ion-title>
+      <basic-form :contentData="babyDetails" :initialData="initialData" @update:selected="handleInputData"
+        @update:inputValue="handleInputData"></basic-form>
+      <div style="display:flex;">
+        <DynamicButton expand="block" @click="confirm()" name="Save" :style="`flex:1`" />
+        <DynamicButton expand="block" @click="onNo()" :style="`flex:1`" name="Cancel" fill="clear" />
+      </div>
+    </ion-content>
+  </ion-modal>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import {
+  IonModal,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonButtons,
+  IonButton,
+  IonContent,
+  IonIcon
+} from '@ionic/vue';
 import { closeOutline } from 'ionicons/icons';
 import DynamicButton from "@/components/DynamicButton.vue";
 import BasicForm from '@/components/BasicForm.vue';
@@ -48,60 +33,60 @@ import { getRadioSelectedValue, modifyCheckboxHeader, modifyFieldValue, modifyRa
 import { YupValidateField } from '@/services/validation_service';
 
 
-  export default defineComponent({
-    name: 'BabyDetailsModal',
-    components: {
-      DynamicButton,
-      IonModal,
-      IonHeader,
-      IonToolbar,
-      IonTitle,
-      IonButtons,
-      IonButton,
-      IonContent,
-      IonIcon,
-      BasicForm
-      },
-      data() {
-          return {    
-              initialData:[]as any  
-        }
+export default defineComponent({
+  name: 'BabyDetailsModal',
+  components: {
+    DynamicButton,
+    IonModal,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonButtons,
+    IonButton,
+    IonContent,
+    IonIcon,
+    BasicForm
+  },
+  data() {
+    return {
+      initialData: [] as any
+    }
+  },
+  props: {
+    isOpen: {
+      type: Boolean as PropType<boolean>,
+      required: true,
+      default: false
     },
-    props: {
-      isOpen: {
-        type: Boolean as PropType<boolean>,
-        required: true,
-        default: false
-      },
-      closeModalFunc : {
-          type: Function as PropType<()=>void>,
-          required: true
-          
-      },
-      onYes : {
-          type: Function as PropType<()=>void>,
-          required: true
-          
-      },
-      onNo : {  
-          type: Function as PropType<()=>void>,
-          required: true
-          
-      },
-      title:{
-          type: String as PropType<string>,
-          required: true
-      }
-      },
-      computed: {
-        ...mapState(useSecondStageOfLabourStore, ["babyDetails"]),
-        ...mapState(useSecondStageOfLabourStore, ["secondStageDetails"]),
-   
+    closeModalFunc: {
+      type: Function as PropType<() => void>,
+      required: true
+
     },
-  
-    watch: {
+    onYes: {
+      type: Function as PropType<() => void>,
+      required: true
+
+    },
+    onNo: {
+      type: Function as PropType<() => void>,
+      required: true
+
+    },
+    title: {
+      type: String as PropType<string>,
+      required: true
+    }
+  },
+  computed: {
+    ...mapState(useSecondStageOfLabourStore, ["babyDetails"]),
+    ...mapState(useSecondStageOfLabourStore, ["secondStageDetails"]),
+
+  },
+
+  watch: {
     secondStageDetails: {
-       async handler() {
+      async handler() {
         this.handleChangeDisplay();
       },
       deep: true,
@@ -109,87 +94,91 @@ import { YupValidateField } from '@/services/validation_service';
     babyDetails: {
       handler() {
         this.handleChangeDisplay();
-        const complications = ['Prematurity','Sepsis','Congenital abnormalities','Asphyxia','Other complications',"Low birthweight"];
-        const managementNewborn = ['Kangaroo mother care','Antibiotics','Other'];
-        // alert("Test")
+        const complications = ['Prematurity', 'Sepsis', 'Congenital abnormalities', 'Asphyxia', 'Other complications', "Low birthweight"];
+        const managementNewborn = ['Kangaroo mother care', 'Antibiotics', 'Other'];
         this.handleNone(complications, 'None');
-        this.handleNone(managementNewborn,'Nothing');
+        this.handleNone(managementNewborn, 'Nothing');
+        this.handleCongenitalCheck();
       },
       deep: true,
     },
   },
-    methods: {
-      async handleInputData(event: any) {
-        YupValidateField(this.babyDetails, BabyDetailsValidationSchema, event.name, event.value);
-      },
-      closeOutline() {
-        return closeOutline;
-      },
-      closeModal() {
-        this.closeModalFunc();
-      },
+  methods: {
+    async handleInputData(event: any) {
+      YupValidateField(this.babyDetails, BabyDetailsValidationSchema, event.name, event.value);
+    },
+    closeOutline() {
+      return closeOutline;
+    },
+    closeModal() {
+      this.closeModalFunc();
+    },
     confirm() {
-        this.onYes();
-        this.babyDetails = [];
-        this.closeModal();
-      },
-      handleChangeDisplay() {
-        const babyGeneralCondition = getRadioSelectedValue(this.secondStageDetails, 'Baby general condition at birth');
-        this.babyDetailsDisplayNone(babyGeneralCondition == "Macerated stillbirth" || babyGeneralCondition == "Fresh stillbirth")
-      
-      },
-      babyDetailsDisplayNone(visibility:boolean) {
-      modifyFieldValue(this.babyDetails,"First name","displayNone", visibility)
-      modifyFieldValue(this.babyDetails,"Last name","displayNone", visibility)
-      modifyFieldValue(this.babyDetails,"Apgar score at 1 minute","displayNone", visibility)
+      this.onYes();
+      this.babyDetails = [];
+      this.closeModal();
+    },
+    handleChangeDisplay() {
+      const babyGeneralCondition = getRadioSelectedValue(this.secondStageDetails, 'Baby general condition at birth');
+      this.babyDetailsDisplayNone(babyGeneralCondition == "Macerated stillbirth" || babyGeneralCondition == "Fresh stillbirth")
+
+    },
+    babyDetailsDisplayNone(visibility: boolean) {
+      modifyFieldValue(this.babyDetails, "First name", "displayNone", visibility)
+      modifyFieldValue(this.babyDetails, "Last name", "displayNone", visibility)
+      modifyFieldValue(this.babyDetails, "Apgar score at 1 minute", "displayNone", visibility)
       modifyFieldValue(this.babyDetails, "Apgar score at 5 minute", "displayNone", visibility)
       modifyRadioValue(this.babyDetails, 'Tetracycline eye ointment given', 'displayNone', visibility);
       modifyRadioValue(this.babyDetails, 'Chlorhexidine', 'displayNone', visibility);
-      modifyCheckboxHeader(this.babyDetails,'Newborn baby complications','displayNone', visibility)
-      modifyCheckboxHeader(this.babyDetails,'Management to newborn','displayNone', visibility)
-      modifyRadioValue(this.babyDetails,'Oxytocin 10 UI given','displayNone', visibility)
-      modifyRadioValue(this.babyDetails,'Vitamin K given','displayNone', visibility)
+      modifyCheckboxHeader(this.babyDetails, 'Newborn baby complications', 'displayNone', visibility)
+      modifyCheckboxHeader(this.babyDetails, 'Management to newborn', 'displayNone', visibility)
+      modifyRadioValue(this.babyDetails, 'Oxytocin 10 UI given', 'displayNone', visibility)
+      modifyRadioValue(this.babyDetails, 'Vitamin K given', 'displayNone', visibility)
     },
-    handleNone(checkBoxes:Array<any>, noneConcept:string){
-      console.log({noneConcept});
+    handleNone(checkBoxes: Array<any>, noneConcept: string) {
       if (getCheckboxSelectedValue(this.babyDetails, noneConcept)?.checked) {
         checkBoxes.forEach((checkbox) => {
-            modifyCheckboxValue(this.babyDetails, checkbox, 'checked', false);
-            modifyCheckboxValue(this.babyDetails, checkbox, 'disabled', true);
+          modifyCheckboxValue(this.babyDetails, checkbox, 'checked', false);
+          modifyCheckboxValue(this.babyDetails, checkbox, 'disabled', true);
         });
-        } else {
+      } else {
         checkBoxes.forEach((checkbox) => {
-            modifyCheckboxValue(this.babyDetails, checkbox, 'disabled', false);
+          modifyCheckboxValue(this.babyDetails, checkbox, 'disabled', false);
         });
-    }
+      }
     },
+    handleCongenitalCheck() {
+      const checked = getCheckboxSelectedValue(this.babyDetails, "Congenital abnormalities")?.checked
+
+      modifyFieldValue(this.babyDetails, "Specify", "displayNone", !checked)
+
     }
+  }
   });
-  </script>
-  
-  <style scoped>
-  ion-modal {
-    --background: rgba(0, 0, 0, 0.9);
-    --width: 100%;
-    --height: 90%;
-    --max-width: 700px;
-    --max-height: 1000px;
-  }
-  
-  ion-header {
-    --background: var(--ion-color-primary);
-  }
-  
-  ion-title {
-    color: var(--ion-color-light);
-  
-  }
-  
-  ion-content {
-    display: flex;
-    /* flex-direction: column; */
-    /* border: black solid 8px; */
-  
-  }
-  </style>
-  
+</script>
+
+<style scoped>
+ion-modal {
+  --background: rgba(0, 0, 0, 0.9);
+  --width: 100%;
+  --height: 90%;
+  --max-width: 700px;
+  --max-height: 1000px;
+}
+
+ion-header {
+  --background: var(--ion-color-primary);
+}
+
+ion-title {
+  color: var(--ion-color-light);
+
+}
+
+ion-content {
+  display: flex;
+  /* flex-direction: column; */
+  /* border: black solid 8px; */
+
+}
+</style>
