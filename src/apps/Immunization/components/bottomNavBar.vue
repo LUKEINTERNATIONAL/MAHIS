b5c417a383d0c231ae831f11e4729144c0da943a
dominickasanga <dominickasanga@gmail.com>
2024-09-20 11:36:12 +0200

    new file:   src/apps/Immunization/components/bottomNavBar.vue

---
+++ src/apps/Immunization/components/bottomNavBar.vue
@@ -0,0 +1,47 @@
+<template>
+
+</template>
+
+<script lang="ts">
+import { IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar, IonRow, IonCol, IonCard } from "@ionic/vue";
+import { defineComponent } from "vue";
+import Toolbar from "@/components/Toolbar.vue";
+
+import SetUser from "@/views/Mixin/SetUser.vue";
+export default defineComponent({
+    name: "Home",
+    mixins: [SetUser],
+    components: {
+        IonContent,
+        IonHeader,
+        IonPage,
+        IonToolbar,
+        Toolbar,
+        IonRow,
+    },
+    data() {
+        return {
+
+        };
+    },
+    computed: {
+
+    },
+    $route: {
+        async handler() {},
+        deep: true,
+    },
+    watch: {
+
+    },
+    async mounted() {
+        
+    },
+    methods: {
+
+    },
+});
+</script>
+
+
+
