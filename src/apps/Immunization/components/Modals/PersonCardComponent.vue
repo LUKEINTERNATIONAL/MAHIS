9aa1e7416397960c5f0426cf7fb79b6da4b6413d
dominickasanga <dominickasanga@gmail.com>
2024-09-20 10:43:27 +0200

    modified:   src/apps/Immunization/components/Modals/PersonCardComponent.vue

--- src/apps/Immunization/components/Modals/PersonCardComponent.vue
+++ src/apps/Immunization/components/Modals/PersonCardComponent.vue
@@ -1,31 +1,31 @@
 <template>
-    <ion-header>
-        <ion-row>
-          <ion-col>
-            <ion-title style="margin-bottom: 1px;" class="modalTitle">{{ heading_text }}</ion-title>
-          </ion-col>
-        </ion-row>
-    </ion-header>
+  <ion-header>
+    <ion-row>
+      <ion-col>
+        <ion-title class="modalTitle">{{ headingText }}</ion-title>
+      </ion-col>
+    </ion-row>
+  </ion-header>
 
-    <ion-content :fullscreen="true" class="ion-padding">
-      <ion-row>
-        <ion-col style="margin-left: 10px; margin-right: 10px;">
-            <BasicInputField
-              placeholder="find client"
-              :icon="searchOutline"
-              :inputValue="search_text"
-              @update:inputValue="searchTextUpdated"
-            />
-            <div>
-                <ion-label v-if="search_txt_error" class="error-label">
-                    {{ 'only letters allowed' }}
-                </ion-label>
-            </div>
-          </ion-col>
-      </ion-row>
+  <ion-content :fullscreen="true" class="ion-padding">
+    <ion-row>
+      <ion-col style="margin-left: 10px; margin-right: 10px;">
+        <BasicInputField
+          placeholder="Find client"
+          :icon="searchOutline"
+          :inputValue="searchText"
+          @update:inputValue="searchTextUpdated"
+        />
+        <div>
+          <ion-label v-if="searchTextError" class="error-label">
+            Only letters allowed
+          </ion-label>
+        </div>
+      </ion-col>
+    </ion-row>
 
-      <ion-row>
-      <ion-col v-for="person in display_pips" :key="person.id">
+    <ion-row>
+      <ion-col v-for="person in displayPeople" :key="person.id">
         <ion-card class="person-card">
           <ion-card-content>
             <div class="card-header">
@@ -33,45 +33,50 @@
                 <ion-card-title>{{ person.given_name }} {{ person.family_name }}</ion-card-title>
                 <ion-label style="font-size: 17px;">{{ getBirthdateAge(person.birthdate) }}</ion-label>
               </div>
-              <ion-icon :icon="eyeOutline" @click="viewPatientProfile(person.patient_id)" class="eye-icon"></ion-icon>
+              <ion-icon :icon="eyeOutline" @click="viewPatientProfile(person.patient_id)" class="eye-icon" aria-label="View patient profile"></ion-icon>
             </div>
             <ion-list lines="none" style="border-radius: 6px;">
               <ion-item class="p-inf">
-                <ion-icon :icon="personOutline" slot="start"></ion-icon>
+                <ion-icon :icon="personOutline" slot="start" aria-hidden="true"></ion-icon>
                 <ion-label> <span class="s-inf">Gender:</span> {{ person.gender }}</ion-label>
               </ion-item>
               <hr class="solid-line">
-
-              <ion-item  class="p-inf">
-                <ion-icon :icon="locationOutline" slot="start"></ion-icon>
+              <ion-item class="p-inf">
+                <ion-icon :icon="locationOutline" slot="start" aria-hidden="true"></ion-icon>
                 <ion-label> <span class="s-inf">Village:</span> {{ person.city_village }}</ion-label>
               </ion-item>
-
             </ion-list>
           </ion-card-content>
         </ion-card>
       </ion-col>
     </ion-row>
-    </ion-content>
+  </ion-content>
 
-    <ion-footer collapse="fade" class="ion-no-border">
-        <ion-row>
-            <ion-col>
-                <ion-button id="cbtn" class="btnText cbtn" fill="solid" style="width: 130px" @click="dismiss()"> Cancel </ion-button>
-            </ion-col>
-        </ion-row>
-    </ion-footer>
-    
- 
+  <ion-footer collapse="fade" class="ion-no-border">
+    <ion-row>
+      <ion-col>
+        <ion-button id="cbtn" class="btnText cbtn" fill="solid" style="width: 130px" @click="dismiss()"> Cancel </ion-button>
+      </ion-col>
+    </ion-row>
+  </ion-footer>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref, computed } from 'vue';
 import { IonGrid, IonRow, IonCol, IonCard, IonContent, IonCardContent, IonFooter, IonCardTitle, IonHeader, IonTitle, IonCardSubtitle, IonList, IonItem, IonLabel, IonIcon, modalController } from '@ionic/vue';
 import { calendarOutline, personOutline, locationOutline, eyeOutline, searchOutline } from 'ionicons/icons';
 import BasicInputField from "@/components/BasicInputField.vue"
 import HisDate from "@/utils/Date";
 
+interface Person {
+  id: string;
+  patient_id: string;
+  given_name: string;
+  family_name: string;
+  birthdate: string;
+  gender: string;
+  city_village: string;
+}
 
 export default defineComponent({
   name: 'PersonCardComponent',
@@ -80,74 +85,71 @@ export default defineComponent({
   },
   props: {
     people: {
-      type: Array as () => any[],
+      type: Array as () => Person[],
       required: true,
     },
-    heading_text: {
+    headingText: {
       type: String,
       required: true,
-      default: "provide heading text",
+      default: "Provide heading text",
     }
   },
-  setup() {
+  setup(props) {
+    const searchText = ref('');
+    const searchTextError = ref(false);
+
+    const displayPeople = computed(() => {
+      if (!searchText.value) return props.people;
+      const nameRegex = new RegExp(searchText.value, 'i');
+      return props.people.filter(person => 
+        nameRegex.test(person.given_name) || nameRegex.test(person.family_name)
+      );
+    });
+
+    const isValidString = (input: string): boolean => {
+      const regex = /^[a-zA-Z\s]*$/;
+      return regex.test(input);
+    };
+
+    const searchTextUpdated = (event: Event) => {
+      const target = event.target as HTMLInputElement;
+      searchText.value = target.value;
+      searchTextError.value = !isValidString(searchText.value);
+    };
+
+    const viewPatientProfile = async (clientId: string) => {
+      try {
+        const modal = await modalController.getTop();
+        if (modal) {
+          modal.dispatchEvent(new CustomEvent('view-client', { detail: { client_id: clientId } }));
+        }
+      } catch (error) {
+        console.error('Error viewing patient profile:', error);
+      }
+    };
+
+    const dismiss = async () => {
+      try {
+        await modalController.dismiss();
+      } catch (error) {
+        console.error('Error dismissing modal:', error);
+      }
+    };
+
     return {
       calendarOutline,
       personOutline,
       locationOutline,
       eyeOutline,
       searchOutline,
-      search_text: '',
-      search_txt_error: false,
-      display_pips: [] as any,
+      searchText,
+      searchTextError,
+      displayPeople,
+      getBirthdateAge: HisDate.getBirthdateAge,
+      searchTextUpdated,
+      viewPatientProfile,
+      dismiss
     };
-  },
-  async mounted() {
-    this.display_pips = this.$props.people
-  },
-  methods: {
-    getBirthdateAge(dateString: string) {
-      return HisDate.getBirthdateAge(dateString)
-    },
-    dismiss() {
-      try {
-        modalController.dismiss()
-      } catch (error) {
-        
-      }
-    },
-    async viewPatientProfile(client_id: string) {
-      // this.$emit('view-client', {client_id})
-      const modal = modalController.getTop();
-      if (modal) {
-          (await modal).dispatchEvent(new CustomEvent('view-client', { detail: {client_id} }));
-      }
-    },
-    isValidString(input: string) {
-      const regex = /^[a-zA-Z\s]*$/;
-      return regex.test(input);
-    },
-    searchTextUpdated(event: any) {
-        const reason = event.target.value
-        this.search_text = reason
-
-        if (this.isValidString(this.search_text) == true) {
-            this.search_txt_error = false
-            this.searchFirstLastName(this.search_text)
-        } else {
-            this.search_txt_error = true
-        }
-        this.$props
-    },
-    searchFirstLastName(srch_str: string) {
-      const people_array_temp = [] as any
-      const nameRegex = new RegExp(srch_str, 'i');
-      this.people.forEach((person: any) => {
-          if (nameRegex.test(person.given_name) == true || nameRegex.test(person.family_name) == true) {
-              people_array_temp.push(person)
-          }
-      })
-      this.display_pips = [];
-    }
   },
 });
 </script>
@@ -179,7 +181,7 @@ ion-card-subtitle {
 
 .eye-icon {
   font-size: 1.8rem;
-  cursor: pointer
+  cursor: pointer;
 }
 
 ion-icon {
@@ -189,25 +191,40 @@ ion-icon {
 ion-item {
   --padding-start: 0;
 }
+
 .error-label {
-    color: #b42318;
-    text-transform: none;
-    float: right;
+  color: #b42318;
+  text-transform: none;
+  float: right;
 }
+
 .p-inf {
   margin-left: 30px;
-  
 }
+
 .solid-line {
   border: none;
-  border-top: 1px solid black; /* Adjust thickness and color */
-  width: 90%;  /* Line length */
-  margin-left: 10px;
-  margin-right: 10px;
-  margin-top: 0px;
-  margin-bottom: 0px;
+  border-top: 1px solid black;
+  width: 90%;
+  margin: 0 10px;
 }
+
 .s-inf {
-  font-size: 16px; color: gray;
+  font-size: 16px; 
+  color: gray;
+}
+
+@media (max-width: 768px) {
+  .person-card {
+    margin-bottom: 8px;
+  }
+
+  ion-card-title {
+    font-size: 1rem;
+  }
+
+  .s-inf {
+    font-size: 14px;
+  }
 }
 </style>
