2dc23056ae59d5eca8301f5d73b98ef46716b2e2
Louis Nyirongo <louisnyirongo73@gmail.com>
2024-09-30 15:39:28 +0200

    patient list service

--- src/services/patient_opd_list.ts
+++ src/services/patient_opd_list.ts
@@ -10,4 +10,26 @@ export class PatientOpdList extends Service {
             startDate
         })
     }
+    static checkOutPatient(visitId:number, closedDateTime:Date){
+        return super.patchJson(`/visits/${visitId}/close`,{
+            visit: { closedDateTime}
+        })
+    }
+    static getCheckInStatus(patientId:number){
+        return super.getJson(`/check_patient_status/${patientId}`)
+    }
+
+    static addPatientToStage(patient_id:number, arrivalTime: Date, stage: 'VITALS' | 'CONSULTATION'|'DISPENSATION'){
+        return super.postJson('/visits', {
+            patient_id,
+            arrivalTime,
+            stage
+        })
+    }
+
+    static getPatientList(stage: 'VITALS' | 'CONSULTATION'|'DISPENSATION'){
+        return super.getJson(`/stages?stage=${stage}`)
+    }
+
+
 }
