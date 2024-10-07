fb9b90a2220577b4cfa989ec523de446136b5d44
petroskayange <kayangepetros@gmail.com>
2024-10-07 15:39:51 +0200

    cleanning the code

--- src/views/Mixin/FormWizard.vue
+++ src/views/Mixin/FormWizard.vue
@@ -1,5 +1,6 @@
 <script lang="ts">
 import Wizard from "form-wizard-vue3";
+import { nextTick } from "vue";
 
 export default {
     name: "App",
@@ -23,11 +24,10 @@ export default {
             if (this.currentTabIndex === 0) {
                 console.log("First Tab");
             }
+
             console.log("All Tabs");
         },
-        wizardCompleted() {
-            console.log("Wizard Completed");
-        },
+
         addHeaderAboveList() {
             const formWizards = document.getElementsByClassName("form-wizard-vue fw-vertical fw-overflow-scroll");
             if (formWizards.length === 0) {
@@ -51,6 +51,26 @@ export default {
             titleLi.style.flexGrow = "0.08";
 
             ulElement.insertBefore(titleLi, ulElement.firstChild);
+        },
+        changeBtnIconPosition() {
+            this.$nextTick(() => {
+                // First, select the button element
+                const button: any = document.querySelector(".fw-footer-left .fw-btn");
+                console.log("🚀 ~ changeBtnIconPosition ~ button:", button);
+
+                // Get the existing span and i elements
+                const span = button.querySelector("span");
+                const icon = button.querySelector("i");
+
+                // Remove both elements from the button
+                button.removeChild(span);
+                button.removeChild(icon);
+
+                // Add them back in the desired order
+                button.appendChild(icon);
+                button.appendChild(span);
+                console.log(this.$refs.target);
+            });
         },
     },
     computed: {
