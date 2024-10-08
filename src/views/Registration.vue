5734d2848ad7ff8946b220b234c74c4fee876e92
petroskayange <kayangepetros@gmail.com>
2024-10-08 06:11:52 +0200

    fixing birth registration not appering in mobile view

--- src/views/Registration.vue
+++ src/views/Registration.vue
@@ -322,7 +322,8 @@ export default defineComponent({
             this.$router.push(url);
         },
         nextStep() {
-            if (this.checkUnderFourteen) this.steps = ["Personal Information", "Location", "Social History", "Guardian Information"];
+            if (this.checkUnderFourteen || this.checkUnderFive)
+                this.steps = ["Personal Information", "Location", "Social History", "Guardian Information"];
             else this.steps = ["Personal Information", "Location", "Guardian Information"];
             const currentIndex = this.steps.indexOf(this.currentStep);
             if (currentIndex < this.steps.length - 1) {
