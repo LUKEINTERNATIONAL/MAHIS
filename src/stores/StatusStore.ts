2253f1940e51bfa0e0e74e713c9c72eb2fe5c76e
petroskayange <kayangepetros@gmail.com>
2024-10-14 01:46:42 +0200

    adding status on store for offline data loading

--- src/stores/StatusStore.ts
+++ src/stores/StatusStore.ts
@@ -3,10 +3,22 @@ import { defineStore } from "pinia";
 export const useStatusStore = defineStore("statusStore", {
     state: () => ({
         apiStatus: true,
+        offlineVillageStatus: {} as any,
+        offlineDistrictStatus: {} as any,
+        offlineTAsStatus: {} as any,
     }),
     actions: {
         setApiStatus(data: any) {
             this.apiStatus = data;
+        },
+        setOfflineDistrictStatus(data: any) {
+            this.offlineDistrictStatus = data;
+        },
+        setOfflineVillageStatus(data: any) {
+            this.offlineVillageStatus = data;
+        },
+        setOfflineTAsStatusStatus(data: any) {
+            this.offlineTAsStatus = data;
         },
     },
     persist: true,
