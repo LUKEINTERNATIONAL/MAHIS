e4add1b3317a1fe35a0b0072ecbcddaf0af826f5
petroskayange <kayangepetros@gmail.com>
2024-10-14 01:18:24 +0200

    updating offline status modal

--- src/components/Modal/OfflineStatus.vue
+++ src/components/Modal/OfflineStatus.vue
@@ -1,9 +1,44 @@
 <template>
-    <div>
-        <k-progress :percent="40" active></k-progress>
-        <k-progress :percent="60" active active-color="#f12711"></k-progress>
-        <k-progress :percent="80" active :color="['#f5af19', '#f12711', '#9254de', '#40a9ff', '#5cdbd3']"></k-progress>
-        <k-progress :percent="100" :color="['#f5af19', '#f12711', '#9254de', '#40a9ff', '#5cdbd3']" :color-flow="true"></k-progress>
+    <div class="" style="display: block">
+        <div class="title">Loading Offline Records</div>
+        <div class="container">
+            <div class="sub_title">
+                Relationships <span class="count"> ({{ offlineVillageStatus?.total_village }}/{{ offlineVillageStatus?.total }})</span>
+            </div>
+            <k-progress
+                :percent="fractionToPercentage(offlineVillageStatus?.total_village, offlineVillageStatus?.total)"
+                active
+                active-color="#fff"
+                color="rgb(107, 199, 107)"
+            ></k-progress>
+            <div class="sub_title">
+                Districts <span class="count"> ({{ offlineDistrictStatus?.total_districts }}/{{ offlineDistrictStatus?.total }})</span>
+            </div>
+            <k-progress
+                :percent="fractionToPercentage(offlineDistrictStatus?.total_districts, offlineDistrictStatus?.total)"
+                active
+                active-color="#fff"
+                color="rgb(107, 199, 107)"
+            ></k-progress>
+            <div class="sub_title">
+                TAs <span class="count"> ({{ offlineTAsStatus?.total_TAs }}/{{ offlineTAsStatus?.total }})</span>
+            </div>
+            <k-progress
+                :percent="fractionToPercentage(offlineTAsStatus?.total_TAs, offlineTAsStatus?.total)"
+                active
+                active-color="#fff"
+                color="rgb(107, 199, 107)"
+            ></k-progress>
+            <div class="sub_title">
+                Villages <span class="count"> ({{ offlineVillageStatus?.total_village }}/{{ offlineVillageStatus?.total }})</span>
+            </div>
+            <k-progress
+                :percent="fractionToPercentage(offlineVillageStatus?.total_village, offlineVillageStatus?.total)"
+                active
+                active-color="#fff"
+                color="rgb(107, 199, 107)"
+            ></k-progress>
+        </div>
     </div>
 </template>
 <script lang="ts">
@@ -44,6 +79,10 @@ import DynamicButton from "@/components/DynamicButton.vue";
 import BasicForm from "@/components/BasicForm.vue";
 import { toastSuccess, toastWarning, popoverConfirmation } from "@/utils/Alerts";
 import { icons } from "@/utils/svg";
+import { text } from "ionicons/icons";
+import { Relationship } from "../../interfaces/relationship";
+import workerData from "@/activate_worker";
+import { useStatusStore } from "@/stores/StatusStore";
 import {
     medkit,
     chevronBackOutline,
@@ -58,6 +97,7 @@ import {
     add,
     person,
 } from "ionicons/icons";
+import get from "localbase/localbase/api/actions/get";
 export default defineComponent({
     name: "StockManagementModal",
     mixins: [SetUser],
@@ -87,26 +127,76 @@ export default defineComponent({
     data() {
         return {
             iconsContent: icons,
-            reportData: [] as any,
-            currentStock: [] as any,
-            stockData: [] as any,
-            allStock: [] as any,
-            outStock: [] as any,
-            filter: "" as any,
-            startDate: HisDate.currentDate(),
-            endDate: HisDate.currentDate(),
-            options: {
-                responsive: true,
-                select: true,
-            } as any,
-            selectedButton: "all",
-            isLoading: false,
-            popoverOpen: false,
-            event: null as any,
-            stockService: {} as any,
-            disabled: false,
+            workerApi: null as any,
+            payload: {} as any,
         };
+    },
+    computed: {
+        ...mapState(useStatusStore, ["offlineVillageStatus", "offlineDistrictStatus", "offlineTAsStatus"]),
+    },
+    watch: {
+        dataToPass: {
+            handler() {
+                this.payload = this.workerApi.data.payload;
+                console.log("🚀 ~ handler ~ this.payload:", this.payload);
+            },
+            deep: true,
+        },
+    },
+    props: {
+        payloadData: {
+            default: {} as any,
+        },
+    },
+    methods: {
+        async getData() {
+            this.workerApi = workerData.workerApi;
+        },
+        fractionToPercentage(numerator: any, denominator: any) {
+            // this.controlModal();
+            if (denominator === 0) {
+                return 0;
+            }
+            let percentage = (numerator / denominator) * 100;
+            return `${percentage.toFixed(2)}`; // Keeps two decimal places
+        },
+        controlModal() {
+            if (
+                this.offlineVillageStatus?.total_village &&
+                this.offlineTAsStatus?.total_TAs &&
+                this.offlineDistrictStatus?.total_districts &&
+                this.offlineVillageStatus?.total_village == this.offlineVillageStatus?.total &&
+                this.offlineDistrictStatus?.total_districts == this.offlineDistrictStatus?.total &&
+                this.offlineTAsStatus?.total_TAs == this.offlineTAsStatus?.total
+            ) {
+                modalController.dismiss();
+            }
+            const status = useStatusStore();
+            status.setOfflineVillageStatus("");
+            status.setOfflineDistrictStatus("");
+            status.setOfflineTAsStatusStatus("");
+        },
     },
 });
 </script>
-<style scoped></style>
+<style scoped>
+.container {
+    padding-top: 30px;
+}
+.title {
+    text-align: center;
+    font-weight: 700;
+    font-size: 18px;
+    color: #00190e;
+    margin-top: 10px;
+}
+.sub_title {
+    font-size: 14px;
+    font-weight: 700;
+}
+.count {
+    padding-top: 3px;
+    padding-left: 3px;
+    font-size: 10px;
+}
+</style>
