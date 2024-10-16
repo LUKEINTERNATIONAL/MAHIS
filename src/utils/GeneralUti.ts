486d8a328f35e02cec45466ee2335d8bec7d1fc2
dominickasanga <dominickasanga@gmail.com>
2024-10-16 21:36:30 +0200

    modified:   src/utils/GeneralUti.ts

--- src/utils/GeneralUti.ts
+++ src/utils/GeneralUti.ts
@@ -173,17 +173,21 @@ export function compareDates(currentDateStr: string, nextAppointmentDateStr: str
 }
 
 export function getGenderCode(input: string): string {
-    const lowercaseInput = input.toLowerCase().trim();
+    try {
+        const lowercaseInput = input.toLowerCase().trim();
   
-    if (lowercaseInput === 'male') {
-      return 'M';
-    } else if (lowercaseInput === 'female') {
-      return 'F';
-    } else if (lowercaseInput === 'm') {
-      return 'Male';
-    } else if (lowercaseInput === 'f') {
-      return 'Female';
-    } else {
-      return 'Invalid gender';
+        if (lowercaseInput === 'male') {
+          return 'M';
+        } else if (lowercaseInput === 'female') {
+          return 'F';
+        } else if (lowercaseInput === 'm') {
+          return 'Male';
+        } else if (lowercaseInput === 'f') {
+          return 'Female';
+        } else {
+          return 'Invalid gender';
+        }
+    } catch (error) {
+        return ''
     }
   }
