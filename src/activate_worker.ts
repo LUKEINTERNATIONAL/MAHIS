0906955563f8f5d96a531d4430b45cc6b5ddd062
petroskayange <kayangepetros@gmail.com>
2024-10-19 19:25:29 +0200

    adding program, user id, and date when activating worker

--- src/activate_worker.ts
+++ src/activate_worker.ts
@@ -1,16 +1,25 @@
 import { useWebWorker } from "@vueuse/core";
 
-const workerApi = useWebWorker("/src/services/worker.ts");
+const workerApi = useWebWorker("src/services/WebWorker/worker.js");
 let url = "";
 let apiKey: any = "";
+let userId: any = "";
+let programId: any = "";
+let date: any = "";
 function updateData() {
     url = `${localStorage.getItem("apiProtocol")}://${localStorage.getItem("apiURL")}:${localStorage.getItem("apiPort")}/api/v1/`;
     apiKey = localStorage.getItem("apiKey");
+    userId = localStorage.getItem("userID");
+    date = localStorage.getItem("sessionDate");
+
+    let program: any = localStorage.getItem("app");
+    program = JSON.parse(program);
+    programId = program ? program.programID : null;
 }
 
 function postData(type: any, payload: any = "") {
     updateData();
-    workerApi.post({ type: type, url: url, apiKey: apiKey, payload });
+    workerApi.post({ type, url, apiKey, userId, programId, date, payload });
 }
 export default {
     workerApi,
