957d7b1a0f6a97111aabeacf0aefa7165dd3e38b
dominickasanga <dominickasanga@gmail.com>
2024-10-21 20:31:33 +0200

    modified:   src/activate_worker.ts modified:   src/services/bmi_service.ts modified:   src/utils/GeneralUti.ts modified:   src/utils/Img.ts modified:   src/validations/StandardValidations.ts modified:   src/views/HomePage.vue

--- src/activate_worker.ts
+++ src/activate_worker.ts
@@ -1,8 +1,8 @@
 import { useWebWorker } from "@vueuse/core";
 import { Service } from "@/services/service";
-import { getBaseURl } from "@/utils/GeneralUti";
+import { getBaseURL } from "@/utils/GeneralUti";
 
-const workerApi = useWebWorker(`/${getBaseURl()}/src/services/WebWorker/worker.js`);
+const workerApi = useWebWorker(`/${getBaseURL()}/src/services/WebWorker/worker.js`);
 let url = "";
 let apiKey: string | null = "";
 let userId: string | null = "";
