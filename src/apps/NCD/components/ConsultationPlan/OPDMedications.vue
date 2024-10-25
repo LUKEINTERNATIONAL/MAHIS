d09d57aff32b37bdd96bbdcb90c167db50aebd4f
Dominic kasanga <dominickasanga@gmail.com>
2024-10-25 12:13:32 +0200

    modified:   src/apps/NCD/components/ConsultationPlan/OPDMedications.vue modified:   src/components/Investigations.vue

--- src/apps/NCD/components/ConsultationPlan/OPDMedications.vue
+++ src/apps/NCD/components/ConsultationPlan/OPDMedications.vue
@@ -1,3 +1,48 @@
 <template>
-    
+
 </template>
+
+<script lang="ts">
+import { IonContent, IonHeader, IonItem, IonList, IonTitle, IonToolbar, IonMenu, IonInput, IonPopover } from "@ionic/vue";
+import { defineComponent, ref } from "vue";
+
+
+export default defineComponent({
+    name: "Menu",
+    components: {
+        IonContent,
+        IonHeader,
+        IonItem,
+        IonList,
+        IonMenu,
+        IonTitle,
+        IonToolbar,
+        IonInput,
+        IonPopover,
+    },
+    data() {
+        return {
+            test: '',
+        };
+    },
+    setup() {
+        return {  };
+    },
+    computed: {
+        // ...mapState(useInvestigationStore, ["investigations"])
+    },
+    watch: {
+        
+    },
+    async mounted() {
+
+    },
+    methods: {
+        
+    },
+})
+</script>
+
+<style scoped>
+    
+</style>
