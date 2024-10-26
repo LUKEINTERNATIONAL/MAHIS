6f00eb27118d2839aefd2424d632a00d86e88358
petroskayange <kayangepetros@gmail.com>
2024-10-26 07:10:54 +0200

    adding get country functionality

--- src/views/Mixin/SetDistricts.vue
+++ src/views/Mixin/SetDistricts.vue
@@ -5,12 +5,14 @@ import { getOfflineRecords } from "@/services/offline_service";
 export default defineComponent({
     data: () => ({
         districtList: [] as any,
+        countriesList: [] as any,
         TAsList: [] as any,
         villageList: [] as any,
         locations: [] as any,
     }),
     async mounted() {
         this.districtList = await getOfflineRecords("districts");
+        this.countriesList = await getOfflineRecords("countries");
         this.TAsList = await getOfflineRecords("TAs");
         this.villageList = await getOfflineRecords("villages");
     },
@@ -24,6 +26,9 @@ export default defineComponent({
         },
         getDistricts(targetId: any) {
             return this.districtList.filter((obj: any) => obj.region_id === targetId);
+        },
+        getCountries(targetId: any) {
+            return this.countriesList.filter((obj: any) => obj.region_id === targetId);
         },
     },
 });
