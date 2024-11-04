08577e8f06e8a3bebfebd51f2c067d8e6d1e29c1
Dominic kasanga <dominickasanga@gmail.com>
2024-11-04 23:21:13 +0200

    modified:   src/apps/NCD/components/ConsultationPlan/NonPharmalogicalTherapyAndOtherNotes.vue

--- src/apps/NCD/components/ConsultationPlan/NonPharmalogicalTherapyAndOtherNotes.vue
+++ src/apps/NCD/components/ConsultationPlan/NonPharmalogicalTherapyAndOtherNotes.vue
@@ -1,68 +1,74 @@
 <template>
-        <div>
+    <ion-card>
+        <ion-card-header>
             <ion-label class="tpStndCls">Non-pharmalogical therapy and other notes</ion-label>
-            <NonPharmacologicalIntervention/>
+        </ion-card-header>
+        <ion-card-content>
+            <div>
+                <NonPharmacologicalIntervention/>
+                
+                <ion-item class="input_item" style="min-height: 120px; margin-top: 14px">
+                    <ion-label> <ion-icon slot="start" :icon="iconsContent.editPen" aria-hidden="true"></ion-icon> </ion-label>
+                    <IonTextarea
+                        @ionInput="validateNotes"
+                        v-model="nonPharmalogicalTherapyAndOtherNotes"
+                        style="min-height: 120px"
+                        class="inputTpln"
+                        :auto-grow="true"
+                        fill="outline"
+                    />
+                </ion-item>
+            </div>
             
-            <ion-item class="input_item" style="min-height: 120px; margin-top: 14px">
-                <ion-label> <ion-icon slot="start" :icon="iconsContent.editPen" aria-hidden="true"></ion-icon> </ion-label>
-                <IonTextarea
-                    @ionInput="validateNotes"
-                    v-model="nonPharmalogicalTherapyAndOtherNotes"
-                    style="min-height: 120px"
-                    class="inputTpln"
-                    :auto-grow="true"
-                    fill="outline"
-                />
-            </ion-item>
-        </div>
-        
-        <div style="margin-top: 14px; margin-left: 10px">
-            <ion-accordion-group ref="accordionGroup" class="previousView">
-                <ion-accordion value="fourth" toggle-icon-slot="start" style="border-radius: 10px; background-color: #fff">
-                    <ion-item slot="header" color="light">
-                        <ion-label class="previousLabel">Previous visits notes</ion-label>
-                    </ion-item>
-                    <div class="ion-padding" slot="content">
-                        <div class="ionLbltp" v-for="(item, index) in FirstPreviousNotes" :key="index">
-                            <div v-if="index == 1">
-                                <div v-for="(item1, index1) in item" :key="index1">
-                                    <div>
-                                        <ion-label class="previousLabelDate">{{ item1.date }}</ion-label>
-                                    </div>
-                                    <div class="previousSecDrgs">
-                                        <ion-list>
-                                            <ion-label class="notes_p">{{ item1.notes }} </ion-label>
-                                        </ion-list>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <ion-accordion-group @ionChange="accordionGroupChangeForNotes">
-                            <ion-accordion value="third" toggle-icon-slot="start" style="border-radius: 10px; background-color: #fff">
-                                <ion-item slot="header" color="light">
-                                    <ion-label class="" color="primary">{{ showMoreNotesMsg }}</ion-label>
-                                </ion-item>
-                                <div class="ion-padding" slot="content">
-                                    <div class="ionLbltp" v-for="(item, index) in RestOfPreviousNotes" :key="index">
-                                        <div v-for="(item1, index1) in removeOuterArray(item)" :key="index1">
-                                            <div>
-                                                <ion-label class="previousLabelDate">{{ item1.date }}</ion-label>
-                                            </div>
-                                            <div class="previousSecDrgs">
-                                                <ion-list>
-                                                    <ion-label class="notes_p">{{ item1.notes }} </ion-label>
-                                                </ion-list>
-                                            </div>
+            <div style="margin-top: 14px; margin-left: 10px">
+                <ion-accordion-group ref="accordionGroup" class="previousView">
+                    <ion-accordion value="fourth" toggle-icon-slot="start" style="border-radius: 10px; background-color: #fff">
+                        <ion-item slot="header" color="light">
+                            <ion-label class="previousLabel">Previous visits notes</ion-label>
+                        </ion-item>
+                        <div class="ion-padding" slot="content">
+                            <div class="ionLbltp" v-for="(item, index) in FirstPreviousNotes" :key="index">
+                                <div v-if="index == 1">
+                                    <div v-for="(item1, index1) in item" :key="index1">
+                                        <div>
+                                            <ion-label class="previousLabelDate">{{ item1.date }}</ion-label>
+                                        </div>
+                                        <div class="previousSecDrgs">
+                                            <ion-list>
+                                                <ion-label class="notes_p">{{ item1.notes }} </ion-label>
+                                            </ion-list>
                                         </div>
                                     </div>
                                 </div>
-                            </ion-accordion>
-                        </ion-accordion-group>
-                    </div>
-                </ion-accordion>
-            </ion-accordion-group>
-        </div>
+                            </div>
+
+                            <ion-accordion-group @ionChange="accordionGroupChangeForNotes">
+                                <ion-accordion value="third" toggle-icon-slot="start" style="border-radius: 10px; background-color: #fff">
+                                    <ion-item slot="header" color="light">
+                                        <ion-label class="" color="primary">{{ showMoreNotesMsg }}</ion-label>
+                                    </ion-item>
+                                    <div class="ion-padding" slot="content">
+                                        <div class="ionLbltp" v-for="(item, index) in RestOfPreviousNotes" :key="index">
+                                            <div v-for="(item1, index1) in removeOuterArray(item)" :key="index1">
+                                                <div>
+                                                    <ion-label class="previousLabelDate">{{ item1.date }}</ion-label>
+                                                </div>
+                                                <div class="previousSecDrgs">
+                                                    <ion-list>
+                                                        <ion-label class="notes_p">{{ item1.notes }} </ion-label>
+                                                    </ion-list>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </ion-accordion>
+                            </ion-accordion-group>
+                        </div>
+                    </ion-accordion>
+                </ion-accordion-group>
+            </div>
+        </ion-card-content>
+    </ion-card>
 </template>
 
 <script lang="ts">
@@ -82,6 +88,10 @@ import {
     IonAccordion,
     IonAccordionGroup,
     AccordionGroupCustomEvent,
+    IonCard,
+    IonCardContent,
+    IonCardHeader,
+    IonIcon
 } from "@ionic/vue";
 
 
