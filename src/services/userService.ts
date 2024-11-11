fdd5ad098dc50687be646d18988009e2a1e698e1
Senzenjani <smanjawira@gmail.com>
2024-11-11 13:44:29 +0200

    Checking app availability before loading location

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -3,10 +3,13 @@ import { LocationService } from "@/services/location_service"
 import { Service } from "@/services/service"
 
 export async function getUserLocation() {
-    const userId: any = Service.getUserID();
-    const user_data = await UserService.getUserByID(userId)
-    if (user_data.location_id != null) {
-            const response = await LocationService.getLocation(user_data.location_id)
-            return response
-    }
+    
+    if (localStorage.getItem("appAvailabilityStatus") !== "offline") {
+        const userId: any = Service.getUserID();
+         const user_data = await UserService.getUserByID(userId)
+        if (user_data.location_id != null) {
+                const response = await LocationService.getLocation(user_data.location_id)
+                return response
+        }
+     }
 }
