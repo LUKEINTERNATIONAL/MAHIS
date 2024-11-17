a7f0b528994784457541d4d02b9cf3cc40e98f22
petroskayange <kayangepetros@gmail.com>
2024-11-17 08:32:06 +0530

    adding start visit button

--- src/components/Programs.vue
+++ src/components/Programs.vue
@@ -1,7 +1,10 @@
 <template>
-    <ion-fab slot="fixed" vertical="bottom" horizontal="end">
-        <ion-fab-button color="primary"> <ion-icon :icon="grid"></ion-icon> </ion-fab-button>
-        <ion-fab-list side="top">
+    <ion-fab slot="fixed" :vertical="verticalPosition" horizontal="end">
+        <ion-fab-button v-if="btn" color="primary" style="--border-radius: 5px; width: 80px; height: 40px; font-weight: 900">
+            Start visit
+        </ion-fab-button>
+        <ion-fab-button v-else color="primary"> <ion-icon :icon="grid"></ion-icon> </ion-fab-button>
+        <ion-fab-list :side="side" :class="btn ? 'btn' : 'fab-list'">
             <ion-fab-button @click="$emit('clicked', btn)" v-for="(btn, index) in programBtn" :key="index" :data-desc="btn.actionName">
                 <ion-icon :icon="add"></ion-icon>
             </ion-fab-button>
@@ -42,6 +45,15 @@ export default defineComponent({
         programBtn: {
             default: [] as any,
         },
+        btn: {
+            default: false,
+        },
+        verticalPosition: {
+            default: "bottom" as any,
+        },
+        side: {
+            default: "top" as any,
+        },
     },
     setup() {
         return {
@@ -66,19 +78,42 @@ ion-fab-button[data-desc] {
     position: relative;
     width: 0px;
 }
-
-ion-fab-button[data-desc]::after {
+/* ion-fab-button[data-desc]::after {
     position: absolute;
     content: attr(data-desc);
     z-index: 1;
     right: -20px;
-    bottom: 7px;
+    color: var(--ion-color-contrast, rgb(112, 109, 109));
+    background-color: rgb(221, 238, 221);
+    padding: 5px 10px;
+    border-radius: 10px;
+    box-shadow: 0 3px 5px -1px rgba(0, 0, 0, 0.2), 0 6px 10px 0 rgba(0, 0, 0, 0.14), 0 1px 18px 0 rgba(0, 0, 0, 0.12);
+}*/
+.fab-list {
+    bottom: 90px;
+}
+.fab-list ion-fab-button[data-desc]::after {
+    right: -29px;
+}
+.btn ion-fab-button[data-desc]::after {
+    right: -70px;
+}
+ion-fab-button[data-desc]::after {
+    position: absolute;
+    content: attr(data-desc);
+    z-index: 1;
     color: var(--ion-color-contrast, rgb(112, 109, 109));
     background-color: rgb(221, 238, 221);
     padding: 5px 10px;
     border-radius: 10px;
     box-shadow: 0 3px 5px -1px rgba(0, 0, 0, 0.2), 0 6px 10px 0 rgba(0, 0, 0, 0.14), 0 1px 18px 0 rgba(0, 0, 0, 0.12);
 }
+.btn {
+    min-width: unset;
+    min-height: unset;
+    top: 5px;
+}
+
 ion-fab-button[data-desc]:hover::after {
     background-color: rgb(200, 230, 200); /* Change background color on hover */
     box-shadow: 0 3px 5px -1px rgba(0, 0, 0, 0.4), 0 6px 10px 0 rgba(0, 0, 0, 0.28), 0 1px 18px 0 rgba(0, 0, 0, 0.24); /* Adjust box-shadow on hover */
