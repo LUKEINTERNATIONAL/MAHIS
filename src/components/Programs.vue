ef5d699947e8e24b2e1798e59790e4d549256d2e
petroskayange <kayangepetros@gmail.com>
2024-11-25 05:30:50 +0200

    positioning start visit btn

--- src/components/Programs.vue
+++ src/components/Programs.vue
@@ -3,7 +3,16 @@
         <ion-fab-button
             v-if="btn"
             color="primary"
-            style="--border-radius: 4px; width: 80px; height: 40px; font-weight: 900; border: 1px solid #cdcdcd; border-radius: 5px"
+            style="
+                --border-radius: 4px;
+                width: 80px;
+                height: 40px;
+                font-weight: 900;
+                top: 20px;
+                position: relative;
+                border: 1px solid #cdcdcd;
+                border-radius: 5px;
+            "
         >
             Start visit
         </ion-fab-button>
