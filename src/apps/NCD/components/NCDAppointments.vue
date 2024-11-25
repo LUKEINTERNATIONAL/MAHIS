de353cf5c9b7024217d0261af49db1168c34c4c9
Dominic kasanga <dominickasanga@gmail.com>
2024-11-25 14:29:56 +0200

    modified:   src/apps/Immunization/services/ncd_appointment_service.ts modified:   src/apps/NCD/components/NCDAppointments.vue

--- src/apps/NCD/components/NCDAppointments.vue
+++ src/apps/NCD/components/NCDAppointments.vue
@@ -1,11 +1,13 @@
 <template>
- dominic
+
 </template>
 
 <script lang="ts">
 import { IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar, IonRow, IonCol, IonCard } from "@ionic/vue";
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import Toolbar from "@/components/Toolbar.vue";
+import HisDate from "@/utils/Date";
+import { Appointment } from "@/apps/Immunization/services/ncd_appointment_service";
 
 import SetUser from "@/views/Mixin/SetUser.vue";
 export default defineComponent({
@@ -20,8 +22,13 @@ export default defineComponent({
         IonRow,
     },
     data() {
+        const startDate = ref(HisDate.currentDate());
+        const endDate = ref(HisDate.currentDate());
+        const people = ref([]) as any;
         return {
-
+            startDate,
+            endDate,
+            people,
         };
     },
     computed: {
@@ -35,10 +42,34 @@ export default defineComponent({
 
     },
     async mounted() {
-        
+        this.getAppointments()
     },
     methods: {
+        formatBirthdate(birthdate: any) {
+            return HisDate.getBirthdateAge(birthdate);
+        },
+        async getAppointments() {
+            const appointments = await Appointment.getDailiyAppointments(
+                this.startDate,
+                this.endDate,
+                ""
+            );
+            appointments.forEach((client: any) => {
+                const apptOb = {
+                    person_id: client.person_id,
+                    npid: client.npid,
+                    appointment_id: 103,
+                    encounter_id: client.encounter_id,
+                    name: client.given_name.concat(" ", client.family_name),
+                    gender: client.gender,
+                    ageDob: this.formatBirthdate(client.birthdate),
+                    village: client.city_village,
+                    appointmentDate: HisDate.toStandardHisDisplayFormat(client.appointment_date),
+                };
+                this.people.push(apptOb);
+            });
 
+        }
     },
 });
 </script>
