59d4f7326bfa6f74e1d676ee33e3bc5ce929958c
petroskayange <kayangepetros@gmail.com>
2024-11-28 05:48:28 +0200

    improving getting of offline data function

--- src/services/offline_service.ts
+++ src/services/offline_service.ts
@@ -1,9 +1,16 @@
 import db from "@/db";
-export async function getOfflineRecords(objStore: any) {
-    return await db
-        .collection(objStore)
-        .get()
-        .then(async (locationData: any) => {
+
+export async function getOfflineRecords(objStore: string, whereClause?: any, returnFirst: boolean = true) {
+    let query = db.collection(objStore);
+
+    if (whereClause) {
+        query = query.doc(whereClause);
+    }
+
+    return await query.get().then(async (locationData: any) => {
+        if (returnFirst) {
             return locationData[0];
-        });
+        }
+        return locationData;
+    });
 }
