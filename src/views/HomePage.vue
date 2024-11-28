f149bc41857e938a12aa05e5842c5804cdeef372
petroskayange <kayangepetros@gmail.com>
2024-11-28 12:59:53 +0200

    improving termination of worker on home page

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -140,7 +140,7 @@ export default defineComponent({
             deep: true,
         },
         workerApi: {
-            handler() {
+            async handler() {
                 const status = useStatusStore();
                 if (this.workerApi?.data?.payload) {
                     if (this.workerApi?.data?.payload?.total_relationships) status.setOfflineRelationshipStatus(this.workerApi?.data?.payload);
@@ -162,6 +162,7 @@ export default defineComponent({
                         this.offlineTAsStatus?.total_TAs == this.offlineTAsStatus?.total
                     ) {
                         modalController.dismiss();
+                        await workerData.terminate();
                     }
                 }
             },
@@ -180,7 +181,6 @@ export default defineComponent({
         await workerData.postData("SET_OFFLINE_LOCATION");
         await workerData.postData("SET_OFFLINE_RELATIONSHIPS");
         await workerData.postData("SYNC_PATIENT_RECORD");
-        await workerData.terminate();
         resetDemographics();
         this.setView();
         await useGlobalPropertyStore().loadGlobalProperty();
