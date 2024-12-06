1a54618e58815e99665f5fe493c3fbe3989fec35
petroskayange <kayangepetros@gmail.com>
2024-12-06 09:24:43 +0200

    getting the first obs offline

--- src/services/offline_service.ts
+++ src/services/offline_service.ts
@@ -14,3 +14,12 @@ export async function getOfflineRecords(objStore: string, whereClause?: any, ret
         return locationData;
     });
 }
+
+export async function getOfflineFirstObsValue(allData: any, value_type: string, concept_id?: number): Promise<string | number | undefined> {
+    // If concept_id is provided, filter the data first
+    const combineArrays = [...allData?.vitals?.saved, ...allData?.vitals?.unsaved];
+    const filteredData = concept_id ? combineArrays.filter((item: any) => item.concept_id === concept_id) : combineArrays;
+
+    // Then sort and return the first item's specified value
+    return filteredData.sort((a: any, b: any) => new Date(b.obs_datetime).getTime() - new Date(a.obs_datetime).getTime())[0]?.[value_type];
+}
