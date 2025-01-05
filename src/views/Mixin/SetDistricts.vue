2b63146518b9d747fda64320152d5effce2aabb5
petroskayange <kayangepetros@gmail.com>
2025-01-05 06:25:48 +0200

    if the server if not done loading data use server data

--- src/views/Mixin/SetDistricts.vue
+++ src/views/Mixin/SetDistricts.vue
@@ -2,6 +2,9 @@
 import { defineComponent } from "vue";
 import { LocationService } from "@/services/location_service";
 import { getOfflineRecords } from "@/services/offline_service";
+import { useStatusStore } from "@/stores/StatusStore";
+import { mapState } from "pinia";
+
 export default defineComponent({
     data: () => ({
         districtList: [] as any,
@@ -13,18 +16,41 @@ export default defineComponent({
     async mounted() {
         await this.getLocationData();
     },
+    computed: {
+        ...mapState(useStatusStore, ["apiStatus"]),
+    },
     methods: {
         async getLocationData() {
             this.districtList = await getOfflineRecords("districts");
             this.countriesList = await getOfflineRecords("countries");
             this.TAsList = await getOfflineRecords("TAs");
             this.villageList = await getOfflineRecords("villages");
+
+            if (this.apiStatus && !this.metaDataRegSyncStatus()) {
+                this.countriesList = await LocationService.getDistricts(4);
+
+                let districtList = [];
+                for (let i of [1, 2, 3]) {
+                    const districts = await LocationService.getDistricts(i);
+                    districtList.push(...districts);
+                }
+                this.districtList = districtList;
+            }
+        },
+        metaDataRegSyncStatus() {
+            return useStatusStore().registrationMetaDataStatus();
         },
         async getVillages(targetId: any) {
+            if (this.apiStatus && !this.metaDataRegSyncStatus()) {
+                return await LocationService.getVillages(targetId);
+            }
             if (this.villageList == "") return null;
             return this.villageList?.filter((obj: any) => obj.traditional_authority_id === targetId);
         },
         async getTAs(targetId: any) {
+            if (this.apiStatus && !this.metaDataRegSyncStatus()) {
+                return await LocationService.getTraditionalAuthorities(targetId);
+            }
             return await getOfflineRecords("TAs", { whereClause: { district_id: targetId } });
         },
         getDistricts(targetId: any) {
