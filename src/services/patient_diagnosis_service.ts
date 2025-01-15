8ca7402cee30367e89eeabb6dc7fd9384518fd77
petroskayange <kayangepetros@gmail.com>
2025-01-15 06:16:18 +0200

    adding await when getting diagnosis

--- src/services/patient_diagnosis_service.ts
+++ src/services/patient_diagnosis_service.ts
@@ -1,18 +1,18 @@
-import { AppEncounterService } from "@/services/app_encounter_service"
-import { ConceptService } from '@/services/concept_service';
+import { AppEncounterService } from "@/services/app_encounter_service";
+import { ConceptService } from "@/services/concept_service";
 
 export class PatientDiagnosisService extends AppEncounterService {
     constructor(patientID: number, providerID: number) {
-        super(patientID, 8, providerID) 
+        super(patientID, 8, providerID);
     }
 
-    static async getDiagnosis(filter='', page=1, limit=10) {
-        const conceptSetId = ConceptService.getConceptID('Qech outpatient diagnosis list')
-        return AppEncounterService.getJson('diagnosis', {
+    static async getDiagnosis(filter = "", page = 1, limit = 10) {
+        const conceptSetId = await ConceptService.getConceptID("Qech outpatient diagnosis list");
+        return AppEncounterService.getJson("diagnosis", {
             id: conceptSetId,
             name: filter,
             page: page,
-            'page_size': limit
-        })
+            page_size: limit,
+        });
     }
 }
