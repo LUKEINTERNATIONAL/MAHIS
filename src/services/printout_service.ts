c20fb91478406d4bb9d384e4420ae1d4a823238f
petroskayange <kayangepetros@gmail.com>
2025-02-19 12:30:21 +0200

    print barcode

--- src/services/printout_service.ts
+++ src/services/printout_service.ts
@@ -27,10 +27,24 @@ export class PrintoutService extends Service {
         super();
     }
 
-    private async _print(labelType: any) {
-        // const eplCommands =  await Service.getText(url);
-        const eplCommands: any = await this.generateEPLLabels("visit");
-        console.log("🚀 ~ PrintoutService ~ _print ~ eplCommands:", eplCommands);
+    private async _print(url: any) {
+        const eplCommands = await Service.getText(url);
+
+        if (!eplCommands) throw new Error("Unable to print Label. Try again later");
+        let printer = await this.getDefaultPrinter();
+        if (isEmpty(printer)) printer = await this.selectDefaultPrinter();
+        if (isEmpty(printer)) throw new Error("No printer device found");
+        if (printer?.port === "BT") return this.printToBluetoothDevice(printer!, eplCommands);
+        return LabelPrinter.printLabel({
+            eplCommands,
+            name: printer.name,
+            address: printer.address,
+            url: await ApiClient.expandPath(url),
+        });
+    }
+    async printData(labelType: any) {
+        const eplCommands = await this.generateEPLLabels(labelType);
+        console.log("🚀 ~ PrintoutService ~ printData ~ eplCommands:", eplCommands);
 
         if (!eplCommands) throw new Error("Unable to print Label. Try again later");
         let printer = await this.getDefaultPrinter();
@@ -51,12 +65,18 @@ export class PrintoutService extends Service {
         let label: any = new ZebraLabel();
         label.setLabelFormat();
         if (labelType == "visit") {
-            label.drawText(`Visit: ${HisDate.toStandardHisDisplayFormat(visits[0])} `, { fontSize: 2 });
+            label.drawText(
+                `VISIT: ${HisDate.toStandardHisDisplayFormat(visits[0])} (${patient.personInformation.given_name} ${
+                    patient.personInformation.family_name
+                })`,
+                { fontSize: 2 }
+            );
             label = await print_diagnosis(label, patient, visits[0]);
             label = await print_prescription(label, patient, visits[0]);
-            label.drawText(`Seen by: ${user.getGivenNameInitial()}.${user.getSurname()} at ${user.userFacilityName}`, { fontSize: 2 });
             // label = print_vitals(label, patient);
+            label.drawText(`Seen by: ${user.getGivenNameInitial()}.${user.getSurname()} at ${user.userFacilityName}`, { fontSize: 2 });
         }
+
         if (labelType == "barcode") label = print_barcode(label);
 
         return label.generateLabel();
