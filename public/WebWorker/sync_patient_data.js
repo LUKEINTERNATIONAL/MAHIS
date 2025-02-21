42626342e10ecd89c8293d75f3339cd717534df7
Dominic Kasanga <dominickasanga@gmail.com>
2025-02-21 17:17:49 +0200

    modified:   public/WebWorker/sync_patient_data.js modified:   src/App.vue modified:   src/apps/NCD/components/PreviousNCDDiagnosis.vue modified:   src/components/Menu.vue

--- public/WebWorker/sync_patient_data.js
+++ public/WebWorker/sync_patient_data.js
@@ -65,6 +65,11 @@ const syncPatientDataService = {
 
     async processPage(previousSyncDate, page) {
         try {
+            console.log("################################");
+            console.log(`Processing page...`);
+
+            console.log(`Page ${page}`);
+
             const pageData = await this.getPatientIds(previousSyncDate, BATCH_SIZE, page);
             if (pageData?.sync_patients?.length) {
                 await this.processBatchWithBuffer(pageData);
--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -9,14 +9,14 @@
       <ion-accordion-group expand="inset" style="margin-inline: unset; margin-top: unset">
         <ion-accordion value="1" @click="navigationMenu('home')" toggle-icon="">
           <ion-item slot="header" color="light">
-            <ion-icon :icon="homeOutline" slot="start"></ion-icon>
+            <ion-icon :icon="homeOutline" slot="start" class="menu-icon"></ion-icon>
             <ion-label class="header lft-drpm">Home</ion-label>
           </ion-item>
         </ion-accordion>
 
         <ion-accordion value="2" @click="navigationMenu('scheduleImmunization')" toggle-icon="">
           <ion-item slot="header" color="light">
-            <ion-icon :icon="calendarOutline" slot="start"></ion-icon>
+            <ion-icon :icon="calendarOutline" slot="start" class="menu-icon"></ion-icon>
             <ion-label class="header lft-drpm">Schedule Immunization</ion-label>
           </ion-item>
         </ion-accordion>
@@ -48,53 +48,79 @@
           </ion-item>
           <div class="content" slot="content">
             <ion-list>
-              <ion-item @click="navigationMenu('locationsManagement')" class="list-content">Location Management</ion-item>
-              <ion-item @click="navigationMenu('OfflineRecords')" class="list-content">Client Management</ion-item>
+              <ion-item @click="navigationMenu('locationsManagement')" class="list-content">
+                <ion-icon :icon="locationOutline" slot="start" class="sub-menu-icon"></ion-icon>
+                <ion-label class="sub-menu-label">Location Management</ion-label>
+              </ion-item>
+              <ion-item @click="navigationMenu('OfflineRecords')" class="list-content">
+                <ion-icon :icon="personOutline" slot="start" class="sub-menu-icon"></ion-icon>
+                <ion-label>Client Management</ion-label>
+              </ion-item>
             </ion-list>
           </div>
         </ion-accordion>
         <ion-accordion value="8">
           <ion-item slot="header" color="light">
-            <ion-icon :icon="barChartOutline" slot="start"></ion-icon>
+            <ion-icon :icon="barChartOutline" slot="start" class="sub-menu-icon"></ion-icon>
             <ion-label class="header lft-drpm">Reports</ion-label>
           </ion-item>
           <div class="ion-padding" slot="content">
             <ion-accordion-group>
               <ion-accordion value="first" v-if="programAttri[2].showReports">
                 <ion-item slot="header">
+                  <ion-icon :icon="medkitOutline" slot="start" class="sub-menu-icon"></ion-icon>
                   <ion-label class="header">OPD Reports</ion-label>
                 </ion-item>
                 <div class="content" slot="content">
                   <ion-list>
-                    <ion-item style="cursor: pointer" @click="navigationMenu('FacilityReports')" class="list-content"
-                    >Facility Reports</ion-item
-                    >
-                    <ion-item style="cursor: pointer" @click="navigationMenu('MoHReports')" class="list-content">MoH</ion-item>
-                    <ion-item style="cursor: pointer" @click="navigationMenu('ClinicalReports')" class="list-content"
-                    >Clinical Reports</ion-item
-                    >
+                    <ion-item style="cursor: pointer" @click="navigationMenu('FacilityReports')" class="list-content">
+                      <ion-icon :icon="businessOutline" class="sub-menu-icon" slot="start"></ion-icon>
+                      <ion-label>Facility Reports</ion-label>
+                    </ion-item>
+                    <ion-item style="cursor: pointer" @click="navigationMenu('MoHReports')" class="list-content">
+                      <ion-icon :icon="flagOutline" slot="start" class="sub-menu-icon"></ion-icon>
+                      <ion-label>MoH</ion-label>
+                    </ion-item>
+                    <ion-item style="cursor: pointer" @click="navigationMenu('ClinicalReports')" class="list-content">
+                      <ion-icon :icon="medkitOutline" slot="start" class="sub-menu-icon"></ion-icon>
+                      <ion-label>Clinical Reports</ion-label>
+                    </ion-item>
                   </ion-list>
                 </div>
               </ion-accordion>
               <ion-accordion value="seventh" v-if="programAttri[1].showReports">
                 <ion-item slot="header">
+                  <ion-icon :icon="heartOutline" slot="start" class="sub-menu-icon"></ion-icon>
                   <ion-label class="header">NCD Reports</ion-label>
                 </ion-item>
                 <div class="content" slot="content">
                   <ion-list>
-                    <ion-item @click="navigationMenu('setSessionDate')" class="list-content">MoH</ion-item>
-                    <ion-item @click="navigationMenu('report')" class="list-content">Clinical Reports</ion-item>
+                    <ion-item @click="navigationMenu('setSessionDate')" class="list-content">
+                      <ion-icon :icon="flagOutline" slot="start" class="sub-menu-icon"></ion-icon>
+                      <ion-label>MoH</ion-label>
+                    </ion-item>
+                    <ion-item @click="navigationMenu('report')" class="list-content">
+                      <ion-icon :icon="medkitOutline" slot="start" class="sub-menu-icon"></ion-icon>
+                      <ion-label>Clinical Reports</ion-label>
+                    </ion-item>
                   </ion-list>
                 </div>
               </ion-accordion>
               <ion-accordion value="third" v-if="programAttri[0].showReports">
                 <ion-item slot="header">
+                  <ion-icon :icon="femaleOutline" slot="start" class="sub-menu-icon"></ion-icon>
                   <ion-label class="header">ANC Reports</ion-label>
                 </ion-item>
                 <div class="content" slot="content">
                   <ion-list>
-                    <ion-item @click="navigationMenu('setSessionDate')" class="list-content">MoH</ion-item>
-                    <ion-item @click="navigationMenu('report')" class="list-content">Clinical Reports</ion-item>
+                    <ion-item @click="navigationMenu('setSessionDate')" class="list-content">
+                      <ion-icon :icon="flagOutline" slot="start" class="sub-menu-icon"></ion-icon>
+                      <ion-label>MoH</ion-label>
+                    </ion-item>
+                    <ion-item @click="navigationMenu('report')" class="list-content">
+                      <ion-icon :icon="medkitOutline" slot="start" class="sub-menu-icon"></ion-icon>
+                      <ion-label>Clinical Reports</ion-label>
+                    </ion-item>
                   </ion-list>
                 </div>
               </ion-accordion>
@@ -105,7 +131,8 @@
                   class="list-content"
                   style="cursor: pointer"
               >
-                EPI Monthly Report
+                <ion-icon :icon="calendarNumberOutline" slot="start" class="sub-menu-icon"></ion-icon>
+                <ion-label>EPI Monthly Report</ion-label>
               </ion-item>
 
               <ion-item
@@ -114,63 +141,56 @@
                   class="list-content"
                   style="cursor: pointer"
               >
-                EIR Overdue Report
+                <ion-icon :icon="alertCircleOutline" slot="start" class="sub-menu-icon"></ion-icon>
+                <ion-label>EIR Overdue Report</ion-label>
               </ion-item>
-
-              <!-- <ion-accordion value="fourth" v-if="programAttri[3].showReports">
-                  <ion-item slot="header">
-                      <ion-label class="header">EIR Reports</ion-label>
-                  </ion-item>
-                  <div class="content" slot="content">
-                      <ion-list>
-                          <ion-item
-                              @click="navigationMenu('EIPMReport')"
-                              class="list-content"
-                              style="cursor: pointer"
-                          >
-                              EPI Monthly Report
-                          </ion-item>
-                          <ion-item
-                              @click="navigationMenu('AEFIReport')"
-                              class="list-content"
-                              style="cursor: pointer"
-                          >
-                              Adverse Events Following Immunization - AEFI
-                          </ion-item>
-                      </ion-list>
-                  </div>
-              </ion-accordion> -->
             </ion-accordion-group>
           </div>
         </ion-accordion>
         <ion-accordion value="9" v-if="isSuperuser" style="border-radius: 0px">
           <ion-item slot="header" color="light">
-            <ion-icon :icon="settingsOutline" slot="start"></ion-icon>
+            <ion-icon :icon="settingsOutline" slot="start" class="sub-menu-icon"></ion-icon>
             <ion-label class="header lft-drpm">Settings</ion-label>
           </ion-item>
           <div class="ion-padding" slot="content">
             <ion-accordion-group>
               <ion-accordion value="first">
                 <ion-item slot="header">
+                  <ion-icon :icon="cogOutline" slot="start" class="sub-menu-icon"></ion-icon>
                   <ion-label class="header">Configurations</ion-label>
                 </ion-item>
                 <div class="content" slot="content">
                   <ion-list>
-                    <ion-item class="list-bottom" @click="navigationMenu('setSessionDate')" value="Session Date">
-                      Session Date
+                    <ion-item class="list-bottom menu-item" @click="navigationMenu('setSessionDate')" value="Session Date">
+                      <ion-icon :icon="calendarOutline" slot="start" class="sub-menu-icon"></ion-icon>
+                      <ion-label>Session Date</ion-label>
                     </ion-item>
-                    <ion-item @click="navigationMenu('clinicaldays')" value="Session Date"> Clinical Days </ion-item>
-                    <ion-item @click="navigationMenu('setDDE')" value="DDE"> DDE </ion-item>
-                    <ion-item class="list-bottom" @click="navigationMenu('setSitePrefix')" value="setSitePrefix">
-                      Site Prefix
+                    <ion-item class="menu-item" @click="navigationMenu('clinicaldays')" value="Session Date">
+                      <ion-icon :icon="medicalOutline" slot="start" class="sub-menu-icon"></ion-icon>
+                      <ion-label>Clinical Days</ion-label>
                     </ion-item>
-                    <ion-item @click="navigationMenu('setPreferences')" value="Preferences"> Preferences </ion-item>
-                    <ion-item @click="navigationMenu('setSmsConfig')" value="SMS" class="list-bottom">Sms Alerts</ion-item>
+                    <ion-item class="menu-item" @click="navigationMenu('setDDE')" value="DDE">
+                      <ion-icon :icon="syncOutline" slot="start" class="sub-menu-icon"></ion-icon>
+                      <ion-label>DDE</ion-label>
+                    </ion-item>
+                    <ion-item class="list-bottom menu-item" @click="navigationMenu('setSitePrefix')" value="setSitePrefix">
+                      <ion-icon :icon="locationOutline" slot="start" class="sub-menu-icon"></ion-icon>
+                      <ion-label>Site Prefix</ion-label>
+                    </ion-item>
+                    <ion-item class="menu-item" @click="navigationMenu('setPreferences')" value="Preferences">
+                      <ion-icon :icon="optionsOutline" slot="start" class="sub-menu-icon"></ion-icon>
+                      <ion-label>Preferences</ion-label>
+                    </ion-item>
+                    <ion-item @click="navigationMenu('setSmsConfig')" value="SMS" class="list-bottom menu-item">
+                      <ion-icon :icon="chatbubbleOutline" slot="start" class="sub-menu-icon"></ion-icon>
+                      <ion-label>Sms Alerts</ion-label>
+                    </ion-item>
                   </ion-list>
                 </div>
               </ion-accordion>
 
               <ion-item button @click="navigationMenu('users')">
+                <ion-icon :icon="peopleOutline" slot="start" class="sub-menu-icon"></ion-icon>
                 <ion-label class="header">User Management</ion-label>
               </ion-item>
             </ion-accordion-group>
@@ -178,7 +198,7 @@
         </ion-accordion>
         <ion-accordion value="10" @click="navigationMenu('managePrinters')" toggle-icon="">
           <ion-item slot="header" color="light">
-            <ion-icon :icon="printOutline" slot="start"></ion-icon>
+            <ion-icon :icon="printOutline" slot="start" class="sub-menu-icon"></ion-icon>
             <ion-label class="header lft-drpm">Manage Printers</ion-label>
           </ion-item>
         </ion-accordion>
@@ -200,6 +220,20 @@ import {
   peopleOutline,
   barChartOutline,
   printOutline,
+  locationOutline,
+  personOutline,
+  medkitOutline,
+  businessOutline,
+  flagOutline,
+  heartOutline,
+  femaleOutline,
+  calendarNumberOutline,
+  alertCircleOutline,
+  cogOutline,
+  medicalOutline,
+  syncOutline,
+  optionsOutline,
+  chatbubbleOutline,
 } from "ionicons/icons";
 import { UserService } from "@/services/user_service";
 import { useRouter } from "vue-router";
@@ -291,6 +325,20 @@ export default defineComponent({
       barChartOutline,
       settingsOutline,
       printOutline,
+      locationOutline,
+      personOutline,
+      medkitOutline,
+      businessOutline,
+      flagOutline,
+      heartOutline,
+      femaleOutline,
+      calendarNumberOutline,
+      alertCircleOutline,
+      cogOutline,
+      medicalOutline,
+      syncOutline,
+      optionsOutline,
+      chatbubbleOutline,
     };
   },
   methods: {
@@ -307,6 +355,35 @@ export default defineComponent({
 });
 </script>
 <style scoped>
+/* Adjust the width of the menu */
+ion-menu {
+  --width: 340px; /* Set the desired width */
+}
+
+/* Optional: Add a max-width for responsiveness */
+@media (max-width: 768px) {
+  ion-menu {
+    --width: 250px; /* Adjust for smaller screens */
+  }
+}
+ion-accordion {
+  margin: 0 auto;
+  border-radius: 0px;
+  background-color: #fff;
+  font-weight: 700;
+}
+ion-item {
+  --background: #fff;
+  --color: #7b7b7b !important;
+}
+ion-label {
+  font-weight: 700;
+  --color: #7b7b7b !important;
+}
+.sub-menu-icon {
+  margin-right: 12px; /* Adjust the value to increase or decrease the gap */
+}
+
 ion-accordion {
   margin: 0 auto;
   border-radius: 0px;
@@ -333,5 +410,12 @@ ion-accordion.accordion-expanded {
 
 .lft-drpm {
   margin-left: 10px;
+}
+
+.menu-item {
+  cursor: pointer;
+  font-size: 15px;
+  font-weight: 600;
+  color: #5A5A5A;
 }
 </style>
